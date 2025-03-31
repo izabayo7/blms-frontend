40693638b5364bf806d334ead18b9c14857507b2
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 13:32:53 +0200

    new: verified

--- src/components/Live/Discussion.vue
+++ src/components/Live/Discussion.vue
@@ -8,7 +8,7 @@
             </div>
             <div class="right">
                 <div class="comment">
-                    <h4 class="comment__name"><span>{{fullNames}} </span><span class="category"><svg height="512pt" viewBox="0 0 512 512" width="512pt" xmlns="http://www.w3.org/2000/svg"><path d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0" fill="#2196f3"/><path d="m385.75 201.75-138.667969 138.664062c-4.160156 4.160157-9.621093 6.253907-15.082031 6.253907s-10.921875-2.09375-15.082031-6.253907l-69.332031-69.332031c-8.34375-8.339843-8.34375-21.824219 0-30.164062 8.339843-8.34375 21.820312-8.34375 30.164062 0l54.25 54.25 123.585938-123.582031c8.339843-8.34375 21.820312-8.34375 30.164062 0 8.339844 8.339843 8.339844 21.820312 0 30.164062zm0 0" fill="#fafafa"/></svg></span></h4>
+                    <h4 class="comment__name"><span>{{fullNames}} </span><span v-for="verified" class="category"><svg height="512pt" viewBox="0 0 512 512" width="512pt" xmlns="http://www.w3.org/2000/svg"><path d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0" fill="#2196f3"/><path d="m385.75 201.75-138.667969 138.664062c-4.160156 4.160157-9.621093 6.253907-15.082031 6.253907s-10.921875-2.09375-15.082031-6.253907l-69.332031-69.332031c-8.34375-8.339843-8.34375-21.824219 0-30.164062 8.339843-8.34375 21.820312-8.34375 30.164062 0l54.25 54.25 123.585938-123.582031c8.339843-8.34375 21.820312-8.34375 30.164062 0 8.339844 8.339843 8.339844 21.820312 0 30.164062zm0 0" fill="#fafafa"/></svg></span></h4>
                     <div class="comment__time"><span>{{elapsedTime}}</span></div>
                     <div class="comment__text"> {{ content.content}}
                     </div>
@@ -18,7 +18,7 @@
                             <p v-else>hide replies</p>
                         </div>
                         <div class="comment-replies" v-if="reply_visible">
-                            <div class="comment-reply" v-for="reply in content.replies" :key="reply._id"><reply-comment :content="reply" /></div>
+                            <div class="comment-reply" v-for="reply in content.replies" :key="reply._id"><reply-comment :verified="reply.sender.category !== 'STUDENT'" :content="reply" /></div>
                         </div>
                     </div>
                     <div class="comment__reply-icon" v-if="!commenting" @click="commenting = !commenting">
@@ -45,7 +45,8 @@
     export default {
         name: "Discussion",
         props:{
-            content:{required:true,type:Object}
+            content:{required:true,type:Object},
+            verified:{default:false,type:Boolean}
         },
         components: {
             ReplyComment,
