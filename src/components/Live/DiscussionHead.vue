036788d010ddf115b4ab1a6e2e3988d9fefb11d2
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:37:14 +0200

    new: styling live card

--- src/components/Live/DiscussionHead.vue
+++ src/components/Live/DiscussionHead.vue
@@ -2,7 +2,7 @@
     <div class="my-discussion-head">
         <div class="discussion-head-container">
             <h1>{{content}}</h1>
-            <div class="live" v-if="live">
+            <div class="live ml-8 px-6" v-if="live">
                 <span>live</span>
             </div>
         </div>
@@ -20,5 +20,21 @@
 </script>
 
 <style lang="scss" scoped>
+    .my-discussion-head{
+        .discussion-head-container{
+            display: flex;
 
+            .live{
+                align-self: center;
+                background-color: $danger;
+                height: fit-content;
+                border-radius: 20px;
+
+                span{
+                    color:$main;
+                    font-size: .8rem;
+                }
+            }
+        }
+    }
 </style>
