d470b7ddc06bdb40ed0965d492da4a092df3e12a
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:37:52 +0200

    recording session depending on scheduler info

--- src/views/live/new/liveClass.vue
+++ src/views/live/new/liveClass.vue
@@ -531,10 +531,7 @@ export default {
         id: 'joinRoom',
         name: this.participationInfo.name,
         room: this.participationInfo.room,
-        /*
-          sudo docker run -it -v /root/kms_videos:/home --rm -p 8888:8888/tcp     -p 5000-5050:5000-5050/udp     -e KMS_MIN_PORT=5000     -e KMS_MAX_PORT=5050     -e KMS_STUN_IP='74.125.143.127'     -e KMS_STUN_PORT=19302    kurento/kurento-media-server:latest
-         */
-        record_session: true,
+        record_session: this.live_session.record_session || false,
         offeringCourse: this.participationInfo.isOfferingCourse
       }
       this.sendMessage(message);
