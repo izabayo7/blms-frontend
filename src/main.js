309c14d9f1d7595a7f1a423df2d0bea4651ab652
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 10:04:05 +0200

    fixed another prob

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'vuetify/dist/vuetify.min.css'
 import './plugins/kurious'
 import './plugins/globalFilters'
 import '@/assets/sass/styles.scss'
