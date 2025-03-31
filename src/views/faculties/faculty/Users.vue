72ce11d67b33dfbd97c75dd5a3e8477aff9dd950
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 17:34:46 +0200

    making users page instead of Student page

---
+++ src/views/faculties/faculty/Users.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="my-faculties bg-one d-flex justify-center">
+  <div class="faculties-container  ">
+      <div class="tabular-users">
+        <div class="table-wrapper mt-6">
+          <div class="table-header">
+            <table-header />
+          </div>
+          <div class="table">
+            <table-ui :data="students" :options="options"/>
+          </div>
+        </div>
+      </div>
+  </div>
+</div>
+</template>
+
+<script>
+import TableHeader from "../../../components/reusable/ui/table-header";
+import TableUi from "../../../components/reusable/ui/table-ui";
+
+export default {
+  //TODO using dynamic students from backend
+name: "FacultyUsers",
+  components: {TableUi, TableHeader,},
+  data(){
+    return{
+      faculty:{},
+      students:[],
+      options:{
+        keysToShow:[ "name",  "published",  "status",  "updatedAt",  "createdAt"]
+      }
+    }
+  },
+  created(){
+    console.log('users')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.my-faculties{
+  height: 100%;
+  width:100%;
+
+  .faculties-container{
+    height: 100%;
+    width:90%;
+
+    .header{
+      @include admin-page-header;
+    }
+
+  }
+}
+</style>
