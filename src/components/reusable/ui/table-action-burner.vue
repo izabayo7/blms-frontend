7bee47d6a909ce329877237b35346d37b1eb0686
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 12:50:49 +0200

    table action burnder creation

---
+++ src/components/reusable/ui/table-action-burner.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="my-table-action-burner">
+  <div class="table-action-burner-container flex">
+    <div class="icon">
+      <div class="icon-wrapper">
+        <slot name="icon"></slot>
+      </div>
+    </div>
+    <div class="text">
+      <h6><slot name="text"></slot></h6>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name: "table-action-burner"
+}
+</script>
+
+<style lang="scss" scoped>
+  .my-table-action-burner{
+    .table-action-burner-container{
+
+    }
+  }
+</style>
