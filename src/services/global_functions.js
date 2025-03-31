dcdb50e0c88716e4c48b77ec0ee7ed976532f8ab
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 17:47:55 +0200

    new: global logout method

--- src/services/global_functions.js
+++ src/services/global_functions.js
@@ -1,4 +1,8 @@
 import moment from 'moment'
+import router from '../router/index'
+import store from '../store/index'
+import vue from '../main'
+
 
 /**
  * object owns a property
@@ -52,9 +56,38 @@ function empty(string){
     return /^\s*$/.test(string);
 }
 
+
+/**
+ * log out user and return him to login
+ */
+function logout(){
+    console.log(router,vue,store)
+
+    // clear the session
+    vue.$session.destroy();
+
+    // reset the modules
+    store.dispatch("user/unsetUser");
+    store.commit("users/RESET_STATE");
+    store.commit("sidebar_navbar/RESET_STATE");
+    store.commit("quiz/RESET_STATE");
+    store.commit("quiz_submission/RESET_STATE");
+    store.commit("notification/RESET_STATE");
+    store.commit("modal/RESET_STATE");
+    store.commit("faculties/RESET_STATE");
+    store.commit("courses/RESET_STATE");
+    store.commit("colleges/RESET_STATE");
+    store.commit("chat/RESET_STATE");
+    store.commit("years/RESET_STATE");
+
+    // redirect to login
+    router.$router.push("/login");
+}
+
 export {
     hasOwn,
     getImgFile,
     elapsedDuration,
-    empty
+    empty,
+    logout,
 }
