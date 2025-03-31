e12bf1c71a01ed23645799312537c2313644f87b
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 16:29:00 +0200

    displaying online users on student side on mobile
    (cherry picked from commit 640338e17daeeaf8da29263a5ca6c1c6ea16b607)

--- src/views/live/new/liveClass.vue
+++ src/views/live/new/liveClass.vue
@@ -230,6 +230,7 @@ openQuiz">
       <div v-else class="live-class--attendance">
         <div class="live-class--attendance--wrapper long">
           <h3>DISCUSSION BOARD </h3>
+          <h3>ONLINE USERS : {{ participants.length }} </h3>
           <div class="live-comments-container viewer">
             <discussion
                 v-for="(comment, i) in comments"
