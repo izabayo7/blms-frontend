68761f4cea7470bd84b56140423042acefff2442
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 09:15:29 +0200

    added mute video

--- src/views/live/new/liveClass.vue
+++ src/views/live/new/liveClass.vue
@@ -65,9 +65,9 @@
               <transition name="fade">
                 <div class="video-controls" v-if="showMenu || noVideo">
                   <div class="video-controls--wrapper">
-                    <button class="start-mute-video">
+                    <button @click="toogleVideo" class="start-mute-video">
                       <span class="icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M16 4a1 1 0 0 1 1 1v4.2l5.213-3.65a.5.5 0 0 1 .787.41v12.08a.5.5 0 0 1-.787.41L17 14.8V19a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h14zm-1 2H3v12h12V6zM7.4 8.829a.4.4 0 0 1 .215.062l4.355 2.772a.4.4 0 0 1 0 .674L7.615 15.11A.4.4 0 0 1 7 14.77V9.23c0-.221.18-.4.4-.4zM21 8.84l-4 2.8v.718l4 2.8V8.84z"/></svg></span>
-                      <span class="text">Video</span>
+                      <span class="text">{{ videoEnabled ? 'Mute video' : 'Unmute video' }}</span>
                     </button>
                     <button class="start-mute-mic">
                       <span class="icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M16.425 17.839A8.941 8.941 0 0 1 13 18.945V23h-2v-4.055A9.004 9.004 0 0 1 3.055 11H5.07a7.002 7.002 0 0 0 9.87 5.354l-1.551-1.55A5 5 0 0 1 7 10V8.414L1.393 2.808l1.415-1.415 19.799 19.8-1.415 1.414-4.767-4.768zm-7.392-7.392l2.52 2.52a3.002 3.002 0 0 1-2.52-2.52zm10.342 4.713l-1.443-1.442c.509-.81.856-1.73.997-2.718h2.016a8.95 8.95 0 0 1-1.57 4.16zm-2.91-2.909l-1.548-1.548c.054-.226.083-.46.083-.703V6a3 3 0 0 0-5.818-1.032L7.686 3.471A5 5 0 0 1 17 6v4a4.98 4.98 0 0 1-.534 2.251z"/></svg></span>
@@ -160,17 +160,22 @@
     return{
       ws:null,
       participants:[],
+      me: null,
       comment:"",
       noVideo:false,
       isPresenting:false,
       participationInfo:{name:"",room:"",isOfferingCourse:false},
       showMenu:false,
+      videoEnabled: true,
+      audioEnabled: true,
       users:[
         {
           img:'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png',
           name:'Ntwari liberi',
           attendance:89,
-        },{
+        },
+          /*
+        {
           img:'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png',
           name:'Ntwari liberi',
           attendance:45,
@@ -294,12 +299,19 @@
           img:'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png',
           name:'Nomi Kazungu',
           attendance:32,
-        },
+        },*/
       ]
     }
   },
   computed:{
     ...mapGetters('user',['user']),
+    // videoStatus(){
+    //   let muted = false;
+    //   if (this.me){
+    //     muted = !this.me.rtcPeer.videoEnabled
+    //   }
+    //   return muted ? 'Mute video' : 'Unmute video'
+    // }
   },
   methods:{
     toggleMenu(status){
@@ -356,6 +368,7 @@
     },
 
     onExistingParticipants(msg) {
+      const self = this
      console.log('existing participant',msg, this.participants)
       let constraints = {
         audio : true,
@@ -379,6 +392,7 @@
 
 
       let participant = new Participant(this.participationInfo.name,this,true);
+
       this.participants[this.participationInfo.name] = participant;
 
 
@@ -394,14 +408,25 @@
           if(error) {
             return console.error(error);
           }
+          self.me = participant;
           this.generateOffer (participant.offerToReceiveVideo.bind(participant));
+
       });
 
 
       msg.data.forEach(console.log)
       msg.data.forEach(this.receiveVideo);
+
     },
 
+    toogleVideo() {
+      this.me.rtcPeer.videoEnabled = !this.me.rtcPeer.videoEnabled;
+      this.videoEnabled = !this.videoEnabled;
+  },
+  toogleAudio() {
+    this.me.rtcPeer.audioEnabled = !this.me.rtcPeer.audioEnabled
+    this.audioEnabled = !this.audioEnabled;
+  },
     leaveRoom() {
        alert('leaving room')
       this.sendMessage({
@@ -504,9 +529,12 @@
     
 
   },
-  mounted(){
-
-  }
+  // watch:{
+  //   me(){
+  //     this.videoEnabled = true
+  //     this.audioEnabled = true
+  //   }
+  // }
 }
 </script>
 
