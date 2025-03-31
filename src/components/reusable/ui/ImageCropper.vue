dc05a4ee66576f5a674360cde1a4c80e84a5689a
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:17:12 +0200

    modifying cropper

--- src/components/reusable/ui/ImageCropper.vue
+++ src/components/reusable/ui/ImageCropper.vue
@@ -2,11 +2,11 @@
 <div class="cropper-container" id="imageCropper" v-if="visible">
   <div class="inner-cropper" >
       <div class="cropper-holder">
-        <cropper class="cropper" :src="img" :stencil-props="{aspectRatio: 1}" @change="change"></cropper>
+        <cropper ref="cropper" class="cropper" :src="img" :stencil-props="{aspectRatio: 1}" ></cropper>
       </div>
-<!--      cropped image preview-->
-      <div class="preview"><img ref="cropper-privew" id="preview-cropped-image" src="" alt=""></div>
   </div>
+  <button class="done" @click="done">Done</button>
+  <button class="cancel" @click="cancel">Cancel</button>
 </div>
 </template>
 
@@ -28,12 +28,16 @@ export default {
     }
   },
   methods:{
-    //liste to change of the cropped area
-    change({canvas}){
-      const image = document.getElementById('preview-cropped-image')
-      image.src = canvas.toDataURL()
+
+    done(){
+      console.log(this.$refs)
+      const { canvas } = this.$refs.cropper.getResult();
       this.$emit('change',canvas.toDataURL()) //emit on component that cropped photo was changed
       emit('image_cropped') //emit globally that image cropped
+      this.visible = false
+    },
+    cancel(){
+      this.visible  = false
     }
   },
   mounted() {
@@ -42,51 +46,54 @@ export default {
     on('new-image-loaded',()=>{
       this.visible = true
     })
-    //listen click on the document
-    document.addEventListener('click',e => {
-      const thisDoc = document.getElementById('imageCropper')
-      const clickInside = thisDoc.contains(e.target) //is what we clicked inside of thisDoc
-
-      //if not inside the make img empty to hide this component
-      if(!clickInside) this.visible = false
-    })
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.cropper{
-  max-height: 100%;
-  height: 30rem;
-}
+
 .cropper-container{
   position: absolute;
   top:50%;
   left:50%;
   transform: translate(-50%,-50%);
   padding:1rem 10rem;
-  height: 21rem;
-  min-width: 20rem;
+  height: fit-content;
+  min-width: 50%;
+  max-width: 80%;
   width: fit-content;
   background-color: $main;
   z-index: 101;
-  box-shadow: 0 0 7px $font;
+  box-shadow:  0 0 20px 4px #4ac1c62e;
 
+  button{
+    box-shadow: 0 0 10px 1px lighten($success,10);
+    width: 10rem;
+    padding:1rem 2rem;
+    background-color: $success;
+    color:$main;
+
+  }
   .inner-cropper{
     height: 100%;
     width: 100%;
     position: relative;
     display: flex;
-    flex-display: row;
+    flex-direction: row;
     justify-content: center;
     align-self: flex-end;
     .cropper-holder{
-      //position: absolute;
-      ////max-width: 10rem;
-      //top:50%;
-      //left:50%;
-      //transform: translate(-50%,-50%);
+        max-height: 50rem;
+        min-height: 70vh;
+        max-width:50rem;
+        min-width: 80%;
+      .cropper{
 
+        .vue-advanced-cropper__image{
+          width:100%;
+          object-fit: contain;
+        }
+      }
     }
     .preview{
       align-self: flex-end;
