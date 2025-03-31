52b2a6a8fdfd48e10e7f4b93249dafb1654769c3
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 16:16:08 +0200

    faculties home page

---
+++ src/views/faculties/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="my-faculties">
+  <div class="faculities-container">
+
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name: "Faculties"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
