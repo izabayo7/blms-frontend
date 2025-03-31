47d4024e925d3325c9c6058ca00da0cd72d0c50e
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 09:25:09 +0200

    creating course-created-card.vue

---
+++ src/components/profile/course-created-card.vue
@@ -0,0 +1,94 @@
+<template>
+<!--course created card-->
+
+  <div class="ccc">
+    <div class="ccc--wrapper d-flex justify-space-between">
+<!--      col 1 with tittle-->
+      <div class="ccc--col ccc--col__1 ccc--title">
+        <div class="ccc--title--name">
+          <h3>Bookkeeping basics</h3>
+        </div>
+        <div class="ccc--title--start-date">
+          <p>Started on 27 jan 2021</p>
+        </div>
+      </div>
+<!--      col 2 with success rate-->
+      <div class="ccc--col ccc--col__2 ccc--class">
+        <div class="ccc--class--name">
+          <p>Communication design year1</p>
+        </div>
+
+      </div>
+<!--      col 3 with last attempted quiz infos-->
+      <div class="ccc--col ccc--col__3 ccc--students">
+        <div class="ccc--students--title">
+          <p>Students</p>
+        </div>
+        <div class="ccc--students--number">
+          <h5>62</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "course-created-card"
+}
+</script>
+
+<style lang="scss" scoped>
+.ccc{
+  max-width:28rem;
+  &--wrapper{
+    padding:.5rem 1rem;
+    border-radius:6.5px;
+  }
+  &--col{
+    &__2{
+
+    }
+  }
+
+  &--title{
+    &--name{
+      font-size:.9rem;
+    }
+    &--start-date{
+      font-size:.6rem;
+    }
+  }
+
+  p {
+    margin-bottom: .5rem;
+  }
+
+  &--class{
+    p{
+      font-size:.75rem;
+      color:lighten($primary,10)
+    }
+
+    &--name{
+      font-size:0.75rem;
+
+    }
+
+  }
+
+  &--last-quiz{
+    p{
+      font-size:.75rem;
+    }
+
+    &--title{
+
+    }
+
+    &--score{
+
+    }
+  }
+}
+</style>
