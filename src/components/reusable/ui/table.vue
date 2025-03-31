bdf7842365a7441da04deb85f2b28b00d3c53a9c
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:09:07 +0200

    creating table

---
+++ src/components/reusable/ui/table.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="my-table">
+  <div class="table-container">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="tabHead in tabHeads" :key="`${tabHead}${Date.now()}` ">{{tabHead}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="content in data" :key="`${content}${Date.now()*Math.random()}`">
+          <td v-for="tabHead in tabHeads" :key="`${tabHead}${Date.now()*Math.random()}` ">{{content[tabHead]}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "table",
+  props:{
+    data:{required:true,type:Array}
+  },
+  computed:{
+    tabHeads(){
+      return Object.keys(this.data)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
