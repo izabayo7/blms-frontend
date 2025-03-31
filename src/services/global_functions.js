92851fff9fb2459f4d439b48414ffb06af112fca
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:28:21 +0200

    new: adding getImgFile function

--- src/services/global_functions.js
+++ src/services/global_functions.js
@@ -4,3 +4,11 @@ exports.hasOwn = (obj, key) => {
         return false
     return Object.prototype.hasOwnProperty.call(obj, key)
 }
+
+//convert base 64 to file image
+exports.getImgFile= async (base64,filename)=>{
+    /* convert base 64 to blob */
+    const blob = await (await fetch(base64)).blob()
+    /* convert blob to file image  and return it*/
+    return  new File([blob],filename,{type:'image/png'})
+}
