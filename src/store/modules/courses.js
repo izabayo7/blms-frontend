c3ef3434c5b3600e467f3623e67f8c97fa95a72b
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 12:11:36 +0200

    fixed course creation process

--- src/store/modules/courses.js
+++ src/store/modules/courses.js
@@ -107,7 +107,7 @@ export default {
             return apis.create('course', course).then(d => {
                 d.data = d.data.data
 
-                courseObject = pick(d.data, ['_id', 'name', 'description', 'faculty_college_year', 'updatedAt'])
+                courseObject = pick(d.data, ['_id', 'name', 'description', 'user_group', 'updatedAt'])
 
                 commit('set_selected_course', d.data._id)
 
