535934395349d1c8e2c72b6b2747b6c596652bc1
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 15:33:50 +0200

    adding search on the page

---
+++ src/views/users/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="my-users-page">
+    <div class="users-page-container bg-one d-flex">
+      <div class="header">
+        <div class="header-wrapper d-flex justify-between">
+          <div class="heading d-flex">
+            <div class="icon">
+              <div class="icon-wrapper">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="25" viewBox="0 0 22 25" fill="none">
+                  <rect x="4.79248" y="0.93927" width="12.4343" height="12.4343" rx="6.21716" fill="#193074"/>
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M8.99325 15.1499C4.22031 15.1499 0.351074 19.0191 0.351074 23.7921V24.9197H21.667V23.7921C21.667 19.0191 17.7978 15.1499 13.0249 15.1499H8.99325Z" fill="#193074"/>
+                </svg>
+              </div>
+            </div>
+            <div class="text-content">
+              <h4>Dashboard</h4>
+              <h2>Users List</h2>
+            </div>
+          </div>
+          <div class="search">
+            <div class="search-wrapper">
+              <search  placeholder="Search user"/>
+            </div>
+          </div>
+          <div class="add-user">
+            <div class="add-user-button">
+              <button-ui>
+                <template #content>Add user</template>
+              </button-ui>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="tabular-users">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import buttonUi from '@/components/reusable/ui/button-ui'
+import Search from '@/components/reusable/Search2'
+export default {
+  name: "Users",
+  components:{buttonUi,Search}
+}
+</script>
+
+<style lang="scss" scoped>
+.my-users-page{
+  height: 100%;
+  .users-page-container{
+    height: 100%;
+
+
+
+  }
+}
+</style>
