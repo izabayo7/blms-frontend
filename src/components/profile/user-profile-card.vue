643e7f6b6ac050dff6dced32efa3604721e680b9
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:45:09 +0200

    using semantic html elements

--- src/components/profile/user-profile-card.vue
+++ src/components/profile/user-profile-card.vue
@@ -1,14 +1,14 @@
 <template>
 <!--  upc = user profile card-->
-  <div class="upc">
-    <div class="upc--wrapper">
-      <div class="upc--information">
-        <div class="upc--information--picture">
+  <section class="upc">
+    <section class="upc--wrapper">
+      <article class="upc--information">
+        <article class="upc--information--picture">
           <figure class="upc--information--picture--img">
             <img src="https://i0.wp.com/www.mobileworldlive.com/wp-content/uploads/2015/10/Dorsey-iamge.png?w=150&ssl=1" alt="user profile picture">
           </figure>
-        </div>
-        <div class="upc--information--data">
+        </article>
+        <article class="upc--information--data">
           <div class="upc--information--data--name">
             <h2>Umuhoza solange</h2>
           </div>
@@ -21,18 +21,18 @@
           <div class="upc--information--data--email">
             <p>solange87@gmail.com</p>
           </div>
-        </div>
-      </div>
-      <div class="upc--cat--buttons">
+        </article>
+      </article>
+      <article class="upc--cat--buttons">
         <div class="upc--cat--message">
           <button-ui> <template #content>Message</template></button-ui>
         </div>
         <div class="upc--cat--call">
           <button-ui> <template #content>Call</template></button-ui>
         </div>
-      </div>
-    </div>
-  </div>
+      </article>
+    </section>
+  </section>
 </template>
 
 <script>
