5865c3b6ffb7927ceb49b0f95cd538d1520543cf
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 17:51:04 +0200

    feat: online user component structure

---
+++ src/components/Live/OnlineUser.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="online-user">
+  <div class="online-users--pic">
+    <figure class="online-user--pic--wrapper">
+      <img src="https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png" alt="">
+    </figure>
+  </div>
+  <div class="online-user--details">
+    <div class="online-user--details--name"></div>
+    <div class="online-user--details--attendency"></div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "OnlineUser",
+  data(){
+    return {
+      user:{}
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
