ea450329aa7bbee3a821f5adc7e9d1ae7337a134
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 13:33:43 +0200

    removed host check

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 
 module.exports = {
+  devServer: {
+    disableHostCheck: true
+  },
   // configureWebpack: {
   //   plugins: [
   //     new VuetifyLoaderPlugin()
