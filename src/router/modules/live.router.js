2a57d111e5bc9dfa95924fc8a0ae7768c80727c6
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 13:47:29 +0200

    feat: constant prefix to all routes

--- src/router/modules/live.router.js
+++ src/router/modules/live.router.js
@@ -1,16 +1,17 @@
 //prefix of the path is "live"
 
-
+const prefix = '/live'
 
 const routes =  [
     {
-        path:'/live/course/{id}',
+        path:'/course/{id}',
         name:"liveCourse",
         component:() => import("@/views/live/new/index.vue")
     }
 ]
 
 
+routes.forEach(route => route.path = `${prefix}${route.path}`)
 
 
 
