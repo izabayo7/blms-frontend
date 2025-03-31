70e89cee0a9ad3e99d928fd9ec4c81494e1c03e5
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 14:52:06 +0200

    changed faculty color and only visible if user is student

--- src/components/profile/user-profile-card.vue
+++ src/components/profile/user-profile-card.vue
@@ -24,7 +24,7 @@
           <div class="upc--information--data--type">
             <h5>{{ user.category }} profile</h5>
           </div>
-          <div class="upc--information--data--faculty">
+          <div v-if="user.category === 'STUDENT'" class="upc--information--data--faculty">
             <h3>{{ user.user_groups }}</h3>
           </div>
           <div class="upc--information--data--email">
@@ -102,7 +102,13 @@ name: "user-profile-card",
       &--faculty{
         margin-top: .5rem;
         h3{
-          color:$color-one;
+          font-family: Roboto;
+          font-style: normal;
+          font-weight: 500;
+          font-size: 15.1822px;
+          line-height: 18px;
+
+          color: #FF7700;
         }
       }
 
