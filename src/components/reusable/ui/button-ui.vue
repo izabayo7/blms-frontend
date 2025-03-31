9e153294b4ea86ed8950fc0f9aa7bfa9f979fa6c
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 09:00:55 +0200

    making button ui

---
+++ src/components/reusable/ui/button-ui.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="my-button">
+  <div @click="clicked" class="button-container bg-color-one px-10 py-3" :class="classList">
+    <button :type="type">
+      <slot name="content"></slot>
+    </button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "button-ui",
+  props:{
+    type:{default:"button"},
+    classList:{default:""},
+  },
+  methods:{
+    clicked(e){
+      this.$emit('click',e)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.my-button{
+  .button-container{
+    border-radius:4.5px;
+
+
+    button{
+      color:$main;
+    }
+  }
+}
+</style>
