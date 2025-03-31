b73ca8bc05512be6dc7ee6a0354c3ed535fdd5a4
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:19:09 +0200

    fixed mobile view in preview.vue and removed unwanted files

---
+++ src/components/shared/notification.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="text-h4 font-weight-bold">{{ title }}</div>
+    <div class="links">
+      <a
+          v-for="(link, i) in links"
+          :key="i"
+          @click="$route.path == link.link ? undefined : $router.push(link.link)"
+      >
+        <span :class="$route.path == link.link ? '' : 'simple_link'">{{
+            link.text
+          }}</span>
+        {{ notLast(i) ? " / " : "" }}
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    links: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    notLast(index) {
+      return parseInt(index) < this.links.length - 1;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.links {
+  a {
+    text-decoration: none;
+    cursor: default;
+  }
+  .simple_link {
+    cursor: pointer;
+  }
+  .simple_link:hover {
+    color: $primary;
+  }
+}
+</style>
