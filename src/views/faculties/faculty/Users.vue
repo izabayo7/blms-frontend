a7e703ab685e5b076d1a331744e4ed33c714c155
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:03:13 +0200

    more dynamic data

--- src/views/faculties/faculty/Users.vue
+++ src/views/faculties/faculty/Users.vue
@@ -6,8 +6,28 @@
           <div class="table-header">
             <table-header />
           </div>
-          <div class="table" v-if="usersOnFaculties.length > 0">
-            <table-ui :data="usersOnFaculties" :options="options"/>
+<!--          <div class="table" v-if="usersOnFaculties.length > 0">-->
+<!--            <table-ui :data="usersOnFaculties" :options="options"/>-->
+<!--          </div>-->
+
+          <div class="table">
+            <table-ui >
+              <template #tableHeaderRow>
+                <table-head-row :cols="options.keysToShow" />
+              </template>
+
+<!--              rows-->
+              <template #tableRows>
+                <table-row :data="user" v-for="user in usersOnFaculties" :key="user._id">
+                  <template #cols>
+                    <td >{{user.sur_name}}</td>
+                    <td>{{user.other_names}}</td>
+                    <td>{{user.email}}</td>
+                    <td>{{user.gender}}</td>
+                  </template>
+                </table-row>
+              </template>
+            </table-ui>
           </div>
         </div>
       </div>
@@ -17,13 +37,15 @@
 
 <script>
 import TableHeader from "../../../components/reusable/ui/table-header";
-import TableUi from "../../../components/reusable/ui/table-ui";
+import TableUi from "../../../components/reusable/table/TableUi";
 import {mapGetters} from "vuex";
+import TableHeadRow from "../../../components/reusable/table/TableHeadRow";
+import TableRow from "../../../components/reusable/table/TableRow";
 
 export default {
   //TODO using dynamic students from backend
 name: "FacultyUsers",
-  components: {TableUi, TableHeader,},
+  components: {TableRow, TableHeadRow, TableUi, TableHeader,},
   data(){
     return{
       students:[],
@@ -38,7 +60,7 @@ name: "FacultyUsers",
     ...mapGetters('users',['usersOnFaculties']),
     faculty(){
       return this.faculties
-    }
+    },
   },
   async mounted(){
 
