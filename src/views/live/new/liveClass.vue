77a3a0a39ad0455b835f6530e772bcc62c29b435
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 14:50:42 +0200

    temporarily removed shareScreen trigger

--- src/views/live/new/liveClass.vue
+++ src/views/live/new/liveClass.vue
@@ -71,7 +71,7 @@
 <!--                          d="M10 7.22L6.603 10H3v4h3.603L10 16.78V7.22zM5.889 16H2a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h3.889l5.294-4.332a.5.5 0 0 1 .817.387v15.89a.5.5 0 0 1-.817.387L5.89 16zm14.525-4l3.536 3.536-1.414 1.414L19 13.414l-3.536 3.536-1.414-1.414L17.586 12 14.05 8.464l1.414-1.414L19 10.586l3.536-3.536 1.414 1.414L20.414 12z"/></svg></span>-->
 <!--                        <span class="text">Mute</span>-->
 <!--                      </button>-->
-                      <button class="start-share-screen" @click="shareScreen">
+                      <button class="start-share-screen">
                       <span class="icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
                                               height="24"><path fill="none" d="M0 0h24v24H0z"/><path
                           d="M9 3v2H4v14h16v-9h2v10a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h6zm9.95 2L16 2.05 17.414.636l5.34 5.34A.6.6 0 0 1 22.33 7H14a2 2 0 0 0-2 2v6h-2V9a4 4 0 0 1 4-4h4.95z"/></svg></span>
