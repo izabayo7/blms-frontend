c061ce827bd35c0b0579dc049bf391470b485c66
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 17:57:26 +0200

    finished schedule popup.vue

--- src/views/live/schedule.vue
+++ src/views/live/schedule.vue
@@ -3,26 +3,26 @@
     <div class="content">
       <div class="heading my-title d-flex my-margin">
         <svg
-          width="50"
-          height="50"
-          viewBox="0 0 50 50"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            viewBox="0 0 50 50"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
         >
-          <circle cx="25" cy="25" r="25" fill="#FC6767" />
+          <circle cx="25" cy="25" r="25" fill="#FC6767"/>
           <path
-            d="M37 20L30 25L37 30V20Z"
-            stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+              d="M37 20L30 25L37 30V20Z"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
           />
           <path
-            d="M28 18H17C15.8954 18 15 18.8954 15 20V30C15 31.1046 15.8954 32 17 32H28C29.1046 32 30 31.1046 30 30V20C30 18.8954 29.1046 18 28 18Z"
-            stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+              d="M28 18H17C15.8954 18 15 18.8954 15 20V30C15 31.1046 15.8954 32 17 32H28C29.1046 32 30 31.1046 30 30V20C30 18.8954 29.1046 18 28 18Z"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
           />
         </svg>
         Start a live class
@@ -30,12 +30,12 @@
       <div class="my-title my-margin">Live class details</div>
       <div class="input-container my-margin">
         <select-ui
-          label="Select course"
-          class="bold-border"
-          name="role"
-          id="user_group"
-          :options="courseNames"
-          @input="
+            label="Select course"
+            class="bold-border"
+            name="role"
+            id="user_group"
+            :options="courseNames"
+            @input="
             (e) => {
               selected_course = e;
             }
@@ -44,12 +44,12 @@
       </div>
       <div class="input-container my-margin">
         <select-ui
-          label="Select chapter"
-          class="bold-border"
-          name="role"
-          id="user_group"
-          :options="chapterNames"
-          @input="
+            label="Select chapter"
+            class="bold-border"
+            name="role"
+            id="user_group"
+            :options="chapterNames"
+            @input="
             (e) => {
               selected_chapter = e;
             }
@@ -62,32 +62,32 @@
             <div class="value">{{ date }}</div>
 
             <svg
-              class="drop-down-icon my-auto ml-auto"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+                class="drop-down-icon my-auto ml-auto"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                d="M9 11H7V13H9V11ZM13 11H11V13H13V11ZM17 11H15V13H17V11ZM19 4H18V2H16V4H8V2H6V4H5C3.89 4 3.01 4.9 3.01 6L3 20C3 21.1 3.89 22 5 22H19C20.1 22 21 21.1 21 20V6C21 4.9 20.1 4 19 4ZM19 20H5V9H19V20Z"
-                fill="black"
+                  d="M9 11H7V13H9V11ZM13 11H11V13H13V11ZM17 11H15V13H17V11ZM19 4H18V2H16V4H8V2H6V4H5C3.89 4 3.01 4.9 3.01 6L3 20C3 21.1 3.89 22 5 22H19C20.1 22 21 21.1 21 20V6C21 4.9 20.1 4 19 4ZM19 20H5V9H19V20Z"
+                  fill="black"
               />
             </svg>
           </div>
           <v-date-picker
-            class="date-picker"
-            v-show="menu"
-            v-model="date"
-            no-title
-            scrollable
+              class="date-picker"
+              v-show="menu"
+              v-model="date"
+              no-title
+              scrollable
           >
             <v-spacer></v-spacer>
-            <v-btn text color="primary" @click="menu = false"> OK </v-btn>
+            <v-btn text color="primary" @click="menu = false"> OK</v-btn>
           </v-date-picker>
         </div>
         <div class="start-now d-flex mb-4 my-margin ml-0 ml-md-7">
-          <input type="checkbox" :v-model="startNow" />
+          <input type="checkbox" :v-model="startNow"/>
           Start Live class now
         </div>
       </div>
@@ -103,27 +103,27 @@
         </div>
         <div ref="timepicker" class="time_pckr">
           <TimePicker
-            @update="updateTime"
-            class="mt-4"
-            v-if="showPicker"
-            format="24hr"
-            scrollable
-            min="9:30"
-            :no-title="true"
-            :use-seconds="useSeconds"
-            max="22:15"
-            @close="showPicker = false"
+              @update="updateTime"
+              class="mt-4"
+              v-if="showPicker"
+              format="24hr"
+              scrollable
+              min="9:30"
+              :no-title="true"
+              :use-seconds="useSeconds"
+              max="22:15"
+              @close="showPicker = false"
           ></TimePicker>
         </div>
       </div>
       <div class="input-container my-margin">
         <div class="label mb-2">Select quiz</div>
         <select-ui
-          class="bold-border"
-          name="role"
-          id="user_group"
-          :options="quizNames"
-          @input="
+            class="bold-border"
+            name="role"
+            id="user_group"
+            :options="quizNames"
+            @input="
             (e) => {
               selected_quiz = e;
             }
@@ -132,19 +132,31 @@
       </div>
       <button class="submit" @click="saveSession()">Schedule class</button>
     </div>
+    <Popup v-show="showModal" :title="'Success'">
+      <template v-slot:content>
+        <div class="subtitle">Live class successfly scheduled for :</div>
+        <div class="detail">course : <span>Mechanical engeneering</span></div>
+        <div class="detail"> user group: <span>ME Year 3</span></div>
+        <div class="detail"><span>APRIL 23 2021 at 18 : 45</span></div>
+        <div class="detail">Student have already been notified</div>
+        <div class="mx-auto"><button @click="showModal = false">OKAY</button></div>
+      </template>
+    </Popup>
   </div>
 </template>
 
 <script>
 import SelectUi from "@/components/reusable/ui/select-ui";
-import { mapActions, mapGetters } from "vuex";
+import {mapActions, mapGetters} from "vuex";
 import TimePicker from "@/components/TimePicker";
+import Popup from "@/components/Live/Popup";
+
 export default {
-  components: { SelectUi, TimePicker },
+  components: {SelectUi, TimePicker, Popup},
   data() {
     return {
       user_group_names: ["Principal", "Instructor", "Student"],
-      date: new Date().toISOString().substring(0,10),
+      date: new Date().toISOString().substring(0, 10),
       menu: false,
       time: "00:00",
       useSeconds: false,
@@ -152,13 +164,14 @@ export default {
       showPicker: false,
       counter: 0,
       selected_course: "",
-      selected_chapter:"",
+      selected_chapter: "",
       selected_quiz: "",
+      showModal: true,
     };
   },
-  watch:{
-    selected_course(){
-      this.selected_chapter=""
+  watch: {
+    selected_course() {
+      this.selected_chapter = ""
     }
   },
   computed: {
@@ -181,7 +194,7 @@ export default {
     chapterNames() {
       let res = [];
       for (const i in this.courses) {
-        if (this.courses[i].name == this.selected_course){
+        if (this.courses[i].name == this.selected_course) {
           for (const k in this.courses[i].chapters) {
             res.push(this.courses[i].chapters[k].name);
           }
@@ -189,7 +202,7 @@ export default {
       }
       return res;
     },
-    quizNames(){
+    quizNames() {
       let res = []
       // for (const i in this.all_quiz) {
       //   res.push(this.quiz[i].name)
@@ -206,10 +219,10 @@ export default {
       if (value) {
         this.time = value;
         if (
-          this.hours &&
-          this.minutes &&
-          this.hours != "00" &&
-          this.minutes != "00"
+            this.hours &&
+            this.minutes &&
+            this.hours != "00" &&
+            this.minutes != "00"
         ) {
           this.showPicker = false;
         }
@@ -223,11 +236,11 @@ export default {
       this.showPicker = true;
     },
     async saveSession() {
-      let chapter_id="";
+      let chapter_id = "";
       for (const i in this.courses) {
-        if (this.courses[i].name == this.selected_course){
+        if (this.courses[i].name == this.selected_course) {
           for (const k in this.courses[i].chapters) {
-            if(this.courses[i].chapters[k].name == this.selected_chapter){
+            if (this.courses[i].chapters[k].name == this.selected_chapter) {
               chapter_id = this.courses[i].chapters[k]._id
             }
           }
@@ -261,11 +274,13 @@ export default {
   background: #f3f6ff;
   padding: 25px 44px;
   min-height: 100%;
+
   .content {
     background: white;
     max-width: 1156px;
     max-height: 745px;
     padding: 58px 62px;
+
     .my-title {
       font-family: Inter;
       font-style: normal;
@@ -276,23 +291,30 @@ export default {
 
       color: #000000;
     }
+
     .my-margin {
       margin-bottom: 50px;
+
       &.input-container {
         margin-bottom: 20px;
       }
     }
+
     .heading {
       place-items: center;
+
       svg {
         margin-right: 15px;
       }
     }
+
     .bold-border .select-container {
       border: 1.54684px solid #bababc !important;
     }
+
     .input-container {
       max-width: 515px;
+
       .label {
         font-family: Inter;
         font-style: normal;
@@ -303,10 +325,12 @@ export default {
         color: #717171;
       }
     }
+
     .select-date {
       border: 1.5px solid lighten($grayish, 20%);
       border-radius: 2px;
       padding: 0.6rem 1.4rem 0.6rem 0;
+
       .value {
         padding-left: 1.4rem;
         font-family: Inter;
@@ -318,6 +342,7 @@ export default {
         color: #7c7c7c;
       }
     }
+
     .date-picker {
       position: relative;
       filter: drop-shadow(2.75622px 2.75622px 6.89055px rgba(25, 48, 116, 0.4));
@@ -325,11 +350,14 @@ export default {
       margin-left: 110px;
     }
   }
+
   .start-now {
     margin-left: 27px;
+
     input {
       margin-right: 11px;
     }
+
     font-family: Inter;
     font-style: normal;
     font-weight: 500;
@@ -339,9 +367,11 @@ export default {
 
     color: #000000;
   }
+
   .y-centered {
     place-items: center;
   }
+
   .text-filed {
     width: 79px;
     height: 41px;
@@ -351,6 +381,7 @@ export default {
     justify-content: center;
     border-radius: 4px;
   }
+
   .submit {
     width: 184px;
     height: 52.67px;
@@ -370,6 +401,7 @@ export default {
     color: #ffffff;
   }
 }
+
 /* Portrait phones and smaller */
 @media (max-width: 1000px) {
   #schedule_live_class {
