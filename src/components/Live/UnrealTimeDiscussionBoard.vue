87b702c4ca32fcb1700f424d7ecf8b0aa100b1b8
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 09:31:22 +0200

    new: new style

--- src/components/Live/UnrealTimeDiscussionBoard.vue
+++ src/components/Live/UnrealTimeDiscussionBoard.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="my-unreal-time-discussion-board">
-        <div class="unreal-time-discussion-bord-container">
-            <div class="student-new-comment">
-                <student-new-comment />
+        <div class="unreal-time-discussion-bord-container px-3 py-5">
+            <div class="head mb-5">
+                <discussion-head content="Economics Basics part II" :live="true"/>
             </div>
-            <!--            <div class="head">-->
-            <!--                <discussion-head />-->
-            <!--            </div>-->
+            <div class="student-new-comment mb-5">
+                <student-new-comment-with-photo />
+            </div>
             <div class="unreal-time-discussions-holder">
                 <!--                <div class="new-comment">-->
                 <!--                    <new-comment />-->
@@ -24,20 +24,25 @@
 
     // import NewComment from "./NewComment";
     import Discussion from "./Discussion";
-    // import DiscussionHead from "./DiscussionHead";
-    import StudentNewComment from "./StudentNewComment";
+    import DiscussionHead from "./DiscussionHead";
+    import StudentNewCommentWithPhoto from "./StudentNewCommentWithPhoto";
 
 
     export default {
         name: "UnrealTimeDiscussionBoard",
-        components:{StudentNewComment,
+        components:{
+            StudentNewCommentWithPhoto,
             // NewComment,
             Discussion,
-            // DiscussionHead
+            DiscussionHead
         }
     }
 </script>
 
 <style lang="scss" scoped>
-
+.my-unreal-time-discussion-board{
+    .unreal-time-discussion-bord-container{
+        background-color: $main;
+    }
+}
 </style>
