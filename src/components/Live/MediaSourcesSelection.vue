e29b4931575772a76e273ee313b7fa07d3618b74
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 16:57:50 +0200

    feat(live): initiation of media sources

---
+++ src/components/Live/MediaSourcesSelection.vue
@@ -0,0 +1,18 @@
+<template>
+<!-- mds => media sources selection-->
+  <div class="mds">
+    <div class="mds--wrapper">
+      media sources
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MediaSourcesSelection"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/live/new/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <media-sources-selection />
+</div>
+</template>
+
+<script>
+import MediaSourcesSelection from "@/components/Live/MediaSourcesSelection";
+export default {
+name: "index.vue",
+  components: {MediaSourcesSelection}
+}
+</script>
+
+<style scoped>
+
+</style>
