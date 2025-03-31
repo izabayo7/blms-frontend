fc805d2030ee9144641ff91098f09431df1400a2
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:14:50 +0200

    removed unused code

--- src/components/dashboard/Navbar.vue
+++ src/components/dashboard/Navbar.vue
@@ -2,28 +2,6 @@
   <div class="my-navbar row">
     <div class="col-6 py-0 col-md-3 d-flex top-left-contents">
       <div class="toggle ml-2 my-auto">
-        <!-- <v-icon large @click="toggle">
-          <svg
-            v-if="1==1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-          >
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z" />
-          </svg>
-          <svg
-            v-else
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-          >
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path d="M3 4h18v2H3V4zm0 7h12v2H3v-2zm0 7h18v2H3v-2z" /></svg
-        >
-        </v-icon> -->
         <button class="toogle" @click="toggle">
           <svg
             v-if="state"
@@ -50,13 +28,6 @@
       <div class="vertically--centered">
         <img :src="college_logo" class="logo" />
       </div>
-    </div>
-    <div
-      :class="`my-search hidden-sm-and-down col-${
-        showCreateCourseButton ? '4' : '5'
-      }`"
-    >
-      <search />
     </div>
     <div :class="`py-0 col-${showCreateCourseButton ? '5' : '4'} d-flex`">
       <div class="hidden-md-and-up vertically--centered">
@@ -148,97 +119,6 @@
         <profile />
       </div>
     </div>
-    <!-- <div class="hidden-md-and-up col-2 vertically--centered">
-      <v-btn
-        dark
-        icon
-        class="add_course mt-3 white--text hidden-lg-and-up"
-        to="/courses/new"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-        >
-          <path fill="none" d="M0 0h24v24H0z" />
-          <path
-            d="M18.031 16.617l4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z"
-            fill="#fff"
-          />
-        </svg>
-      </v-btn>
-    </div>
-    <div
-      v-if="showCreateCourseButton"
-      class="create_course col-2 col-md-1 vertically--centered"
-    >
-      <v-btn
-        rounded
-        dark
-        class="add_course mt-3 white--text hidden-md-and-down"
-        to="/courses/new"
-      >
-        <svg
-          id="add-line"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path
-            id="Path_1632"
-            data-name="Path 1632"
-            d="M0,0H24V24H0Z"
-            fill="none"
-          />
-          <path
-            id="Path_1633"
-            data-name="Path 1633"
-            d="M11,11V5h2v6h6v2H13v6H11V13H5V11Z"
-            fill="#fff"
-          />
-        </svg>
-        Create new course
-      </v-btn>
-      <v-btn
-        dark
-        icon
-        class="add_course mt-3 white--text hidden-lg-and-up"
-        to="/courses/new"
-      >
-        <svg
-          id="add-line"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path
-            id="Path_1632"
-            data-name="Path 1632"
-            d="M0,0H24V24H0Z"
-            fill="none"
-          />
-          <path
-            id="Path_1633"
-            data-name="Path 1633"
-            d="M11,11V5h2v6h6v2H13v6H11V13H5V11Z"
-            fill="#fff"
-          />
-        </svg>
-      </v-btn>
-    </div>
-    <div
-      :class="`notification   col-1 ${
-        !showCreateCourseButton ? '' : 'offset-lg-1'
-      }`"
-    >
-      <notifications />
-    </div>
-    <div class="profile col-2">
-      <profile />
-    </div> -->
   </div>
 </template>
 <script>
@@ -247,7 +127,6 @@ import { mapMutations, mapState } from "vuex";
 export default {
   name: "Navbar",
   components: {
-    search: () => import("@/components/reusable/Search"),
     notifications: () => import("@/components/reusable/Notification"),
     profile: () => import("./Profile"),
   },
--- src/components/reusable/Search2.vue
+++ src/components/reusable/Search2.vue
@@ -1,75 +1,89 @@
 <template>
-<div class="my-search">
-  <div class="search-container">
-    <form action="" @submit.prevent="search" class="d-flex ">
-      <div class="input-wrapper col-10">
-        <label for="search">
-           <input :placeholder="placeholder" v-model="content" type="text" id="search">
-        </label>
-      </div>
-      <div class="icon col-2 flex justify-end">
-        <button type="submit" class="icon-wrapper" @click="search">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none">
-           <path d="M12.4472 10.1758H11.7199L11.4622 9.92725C12.3643 8.87779 12.9075 7.51534 12.9075 6.0332C12.9075 2.72832 10.2286 0.0494385 6.92371 0.0494385C3.61883 0.0494385 0.939941 2.72832 0.939941 6.0332C0.939941 9.33808 3.61883 12.017 6.92371 12.017C8.40584 12.017 9.7683 11.4738 10.8178 10.5717L11.0663 10.8294V11.5567L15.6692 16.1504L17.0409 14.7787L12.4472 10.1758ZM6.92371 10.1758C4.63146 10.1758 2.7811 8.32545 2.7811 6.0332C2.7811 3.74096 4.63146 1.8906 6.92371 1.8906C9.21595 1.8906 11.0663 3.74096 11.0663 6.0332C11.0663 8.32545 9.21595 10.1758 6.92371 10.1758Z" fill="black"/>
-          </svg>
-        </button>
-      </div>
-    </form>
+  <div class="my-search">
+    <div class="search-container">
+      <form action="" @submit.prevent="search" class="d-flex">
+        <div class="input-wrapper col-10">
+          <label for="search">
+            <input
+              :placeholder="placeholder"
+              v-model="content"
+              type="text"
+              id="search"
+            />
+          </label>
+        </div>
+        <div class="icon col-2 flex justify-end">
+          <button type="submit" class="icon-wrapper" @click="search">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="17"
+              viewBox="0 0 18 17"
+              fill="none"
+            >
+              <path
+                d="M12.4472 10.1758H11.7199L11.4622 9.92725C12.3643 8.87779 12.9075 7.51534 12.9075 6.0332C12.9075 2.72832 10.2286 0.0494385 6.92371 0.0494385C3.61883 0.0494385 0.939941 2.72832 0.939941 6.0332C0.939941 9.33808 3.61883 12.017 6.92371 12.017C8.40584 12.017 9.7683 11.4738 10.8178 10.5717L11.0663 10.8294V11.5567L15.6692 16.1504L17.0409 14.7787L12.4472 10.1758ZM6.92371 10.1758C4.63146 10.1758 2.7811 8.32545 2.7811 6.0332C2.7811 3.74096 4.63146 1.8906 6.92371 1.8906C9.21595 1.8906 11.0663 3.74096 11.0663 6.0332C11.0663 8.32545 9.21595 10.1758 6.92371 10.1758Z"
+                fill="black"
+              />
+            </svg>
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
   name: "Search2",
-  props:{
-    placeholder:{default:"Search ..."}
+  props: {
+    placeholder: { default: "Search ..." },
   },
-  data(){
-    return{
-      content:""
-    }
+  data() {
+    return {
+      content: "",
+    };
   },
-  methods:{
-    search(){
-      this.$emit('search',this.content)
-    }
-  }
-}
+  methods: {
+    search() {
+      this.$emit("search", this.content);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.my-search{
-  .search-container{
-    form{
-      background:$main;
-      border-radius:5px;
-      width:100%;
-      border:1px solid $color-grey;
-      .input-wrapper{
-        padding:0 0 0 1rem;
+.my-search {
+  .search-container {
+    form {
+      background: $main;
+      border-radius: 5px;
+      width: 100%;
+      border: 1px solid $color-grey;
+      .input-wrapper {
+        padding: 0 0 0 1rem;
 
-        input{
-          height:100%;
-          width:100%;
+        input {
+          height: 100%;
+          width: 100%;
         }
       }
-      .icon{
-        padding:0 0 0 0;
+      .icon {
+        padding: 0 0 0 0;
 
-        button{
-          background:none;
-          border:none;
-          display:flex;
-          justify-self:end;
-          justify-content:center;
-          align-items:center;
-          width:100%;
+        button {
+          background: none;
+          border: none;
+          display: flex;
+          justify-self: end;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
           height: 2.7rem;
-          border-radius:5px;
+          border-radius: 5px;
 
-          &:hover{
-            background:lighten($color-grey,10);
+          &:hover {
+            background: lighten($color-grey, 10);
           }
         }
       }
