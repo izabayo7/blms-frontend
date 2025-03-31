98fd3b667f0576d2ba6346bc3c47a7d834391e14
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 12:22:27 +0200

    showing simple loader

--- src/components/reusable/user-simple-card.vue
+++ src/components/reusable/user-simple-card.vue
@@ -1,40 +1,47 @@
 <template>
 <article class="user-card">
-  <article class="user-card--wrapper">
-    <div class="user-card--row user-card--info user-card--row__1 d-flex">
-      <div class="user-card--info--col user-card--info--names-and-type user-card--info--col__1 col-8">
-        <h2> <slot name="name"></slot> </h2>
-        <h6> <slot name="type"></slot> </h6>
-      </div>
-      <div class="user-card--info--col user-card--info--pic user-card--info--col__2 col-4">
-        <div class="image--wrapper">
-          <slot name="image"></slot>
+
+  <article class="user-card--wrapper" >
+    <div class="loader" v-if="loading">
+      <p>Loading</p>
+    </div>
+    <div v-else>
+      <div class="user-card--row user-card--info user-card--row__1 d-flex">
+        <div class="user-card--info--col user-card--info--names-and-type user-card--info--col__1 col-8">
+          <h2> <slot name="name"></slot></h2>
+          <h6> <slot name="type"></slot> </h6>
+        </div>
+        <div class="user-card--info--col user-card--info--pic user-card--info--col__2 col-4">
+          <div class="image--wrapper">
+            <slot name="image"></slot>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="user-card--row user-card--actions user-card--row__2 d-flex">
-      <div class="user-card--actions--col user-card--actions--col__2">
-        <router-link to="">
-          <button-ui rounded fill :class-list="'px-4 py-2'" class="user-card--actions--button__message user-card--actions--button">
-            <template #content>
-              <span>Message</span>
-            </template>
-          </button-ui>
-        </router-link>
-      </div>
+      <div class="user-card--row user-card--actions user-card--row__2 d-flex">
+        <div class="user-card--actions--col user-card--actions--col__2">
+          <router-link to="">
+            <button-ui rounded fill :class-list="'px-4 py-2'" class="user-card--actions--button__message user-card--actions--button">
+              <template #content>
+                <span>Message</span>
+              </template>
+            </button-ui>
+          </router-link>
+        </div>
 
-      <div class="user-card--actions--col user-card--actions--col__1 ">
-        <router-link to="">
-          <button-ui rounded :class-list="'px-2 py-2'"  class="user-card--actions--button__visit-profile user-card--actions--button">
-            <template #content>
-              <span>Visit profile</span>
-            </template>
-          </button-ui>
-        </router-link>
-      </div>
+        <div class="user-card--actions--col user-card--actions--col__1 ">
+          <router-link to="">
+            <button-ui rounded :class-list="'px-2 py-2'"  class="user-card--actions--button__visit-profile user-card--actions--button">
+              <template #content>
+                <span>Visit profile</span>
+              </template>
+            </button-ui>
+          </router-link>
+        </div>
 
+      </div>
     </div>
   </article>
+
 </article>
 </template>
 
@@ -42,6 +49,9 @@
 import ButtonUi from "./ui/button-ui";
 export default {
   name: "user-simple-card",
+  props:{
+    loading:{type:Boolean,default:false}
+  },
   components: {ButtonUi}
 }
 </script>
