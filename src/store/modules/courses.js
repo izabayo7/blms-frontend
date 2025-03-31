04fb759b7239cf287526548ab695eca077cc72f6
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 12:00:44 +0200

    added new libeli mutations in courses.js

--- src/store/modules/courses.js
+++ src/store/modules/courses.js
@@ -10,6 +10,7 @@ const getDefaultState = () => ({
     selectedCourse: '',
     // the current chapter's id (the one we are viewing or editing)
     selectedChapter: '',
+    totalCommentsOnAChapter: ""   //total number of comments based on chapter
 })
 
 export default {
@@ -23,6 +24,14 @@ export default {
         // update the selectedChapter
         set_selected_chapter(state, id) {
             state.selectedChapter = id
+        },
+        // update number of total comments
+        SET_TOTAL_COMMENTS_ON_A_CHAPTER(state, num) {
+            state.totalCommentsOnAChapter = num
+        },
+        // update number of total comments
+        DELETE_TOTAL_COMMENTS_ON_A_CHAPTER(state) {
+            state.totalCommentsOnAChapter = ""
         },
         // initialise a new chapter
         initialise_new_chapter(state) {
