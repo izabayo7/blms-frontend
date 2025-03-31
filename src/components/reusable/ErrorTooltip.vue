08b3d81209f3cf6027bb07dc96012e2eef49d1d0
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 15:23:02 +0200

    new: initiating tooltip

---
+++ src/components/reusable/ErrorTooltip.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-error-tooltip">
+    <div class="error-tooltip-container">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "ErrorTooltip"
+}
+</script>
+
+<style scoped>
+
+</style>
