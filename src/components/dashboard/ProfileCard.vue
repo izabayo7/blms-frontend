780077257f94dc6b1a1c2714cf94e722c012bc5b
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 09:43:31 +0200

    new: initiating ProfileCard.vue

---
+++ src/components/dashboard/ProfileCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="my-profile-card">
+        <div class="profile-card-container">
+            <div class="profile-information"></div>
+            <div class="profile-actions"></div>
+            <div class="logout"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProfileCard"
+    }
+</script>
+
+<style scoped>
+
+</style>
