8bdeef27b0133447d2a1960a5434c6a3e0bb573f
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 14:03:24 +0200

    creating subpage-navbar.vue

---
+++ src/components/reusable/subpage-navbar.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="subpage-navbar">
+  <div class="subpage-navbar--wrapper">
+    <ul class="subpage-navbar--links">
+      <li class="subpage-navbar-link">Faculty Overview</li>
+      <li class="subpage-navbar-link">Users</li>
+      <li class="subpage-navbar-link">Student Groups</li>
+      <li class="subpage-navbar-link">Adminstration</li>
+    </ul>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name: "subpage-navbar"
+}
+</script>
+
+<style lang="scss" scoped>
+.subpage-navbar{
+  &--wrapper{
+
+  }
+  &--links{
+
+  }
+  &--link{
+    text-transform: uppercase;
+  }
+}
+</style>
