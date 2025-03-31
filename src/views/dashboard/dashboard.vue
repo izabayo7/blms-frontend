97581d9a974e3b853ae56b806bb03efc4bd4d8d7
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 09:39:38 +0200

    added google analytics to the site

--- src/views/dashboard/dashboard.vue
+++ src/views/dashboard/dashboard.vue
@@ -58,6 +58,11 @@ export default {
   },
   mixins: [userSimpleCard, userPayment],
   created() {
+    this.$gtag.event('dashboard-visit', {
+      'event_category': 'logs',
+      'event_label': 'User visited the dashboard',
+      'value': 1
+    })
     this.redirect()
   },
   computed: {
