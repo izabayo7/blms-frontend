a0757564460cfdbd9a94698c946622b8bfacfa03
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 15:27:14 +0200

    feat(input): finalising

--- src/components/reusable/ui/ExpandableInput.vue
+++ src/components/reusable/ui/ExpandableInput.vue
@@ -1,11 +1,25 @@
 <template>
-  <span role="textbox" contenteditable type="text">
+  <span ref="editable" @input="input" role="textbox" contenteditable type="text">
   </span>
 </template>
 
 <script>
 export default {
-  name: "ExpandableInput"
+  name: "ExpandableInput",
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+  method:{
+    input(e){
+      this.$emit('input',e.target.value)
+    }
+  },
+  mounted(){
+    this.$refs.editable.innerText = this.value;
+  }
 }
 </script>
 
