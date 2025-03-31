4833726dd2b9e6ec58aa207d1c91644aebb4b525
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 14:09:20 +0200

    added way to limit chapters instructor side

--- src/store/modules/courses.js
+++ src/store/modules/courses.js
@@ -45,7 +45,8 @@ export default {
                         name: '',
                         description: '',
                         quiz: [],
-                        attachments: []
+                        attachments: [],
+                        status: 1
                     })
                 }
             }
