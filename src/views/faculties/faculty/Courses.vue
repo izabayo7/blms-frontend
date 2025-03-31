0b1d874096264dd5b73c9b0bcfa7a5c65ad3c03f
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:30:50 +0200

    showing user links

--- src/views/faculties/faculty/Courses.vue
+++ src/views/faculties/faculty/Courses.vue
@@ -2,16 +2,15 @@
 <div class="my-faculties bg-one d-flex justify-center">
   <div class="faculties-container  ">
       <div class="tabular-users">
-        <div class="table-wrapper mt-6" v-if="courses.length > 0">
+        <div class="table-wrapper mt-6" v-if="coursesByFaculty.length > 0">
 
 <!--          table header-->
           <div class="table-header">
             <table-header />
           </div>
-
 <!--          list of courses in table form-->
           <div class="table">
-            <table-ui :options="options" :data="courses"/>
+            <table-ui :options="options" :data="coursesByFaculty"/>
           </div>
         </div>
       </div>
@@ -24,6 +23,7 @@ import TableHeader from "../../../components/reusable/ui/table-header";
 import TableUi from "../../../components/reusable/ui/table-ui";
 import {mapGetters} from "vuex";
 
+
 export default {
   //TODO using dynamic students from backend
 name: "FacultyCourses",
@@ -31,13 +31,15 @@ name: "FacultyCourses",
   data(){
     return{
       courses:[],
+      facultyId:this.$route.params.facultyId,
       options:{
-        keysToShow:[ "name",  "published",  "status",  "updatedAt",  "createdAt"]
+        keysToShow:[ "name",  "published",  "status"]
       }
     }
   },
   computed:{
     ...mapGetters('faculties',['faculties']),
+    ...mapGetters('courses',['coursesByFaculty']),
     faculty(){
       return this.faculties
     }
@@ -45,8 +47,14 @@ name: "FacultyCourses",
   methods:{
 
   },
-  mounted(){
-    this.$store.dispatch('faculties/changeHeader',{head:this.faculty.name,title:"Courses List"})
+  async mounted(){
+
+      if(this.faculty.length <= 0)
+        await this.$store.dispatch("faculties/getFaculties",this.facultyId)
+
+
+    await this.$store.dispatch('courses/getCourseByFaculty',{facultyId:this.facultyId})
+    await this.$store.dispatch('faculties/changeHeader',{head:this.faculty.name,title:"Courses List"})
 
   }
 }
