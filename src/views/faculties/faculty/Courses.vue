f32e1d52e4a7b09994fcc02d231b5e2d77dfb0c1
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 10:45:36 +0200

    removing static header

--- src/views/faculties/faculty/Courses.vue
+++ src/views/faculties/faculty/Courses.vue
@@ -1,38 +1,6 @@
 <template>
 <div class="my-faculties bg-one d-flex justify-center">
   <div class="faculties-container  ">
-      <div class="header">
-        <div class="header-wrapper d-flex ">
-          <div class="heading col d-flex align-center">
-            <div class="icon">
-              <div class="icon-wrapper">
-                <svg xmlns="http://www.w3.org/2000/svg" width="49" height="38" viewBox="0 0 49 38" fill="none">
-                  <rect x="36.2266" y="10.3643" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M32.916 28.3897H48.8837C48.5444 24.2775 45.0995 21.046 40.8998 21.046C36.7002 21.046 33.2553 24.2775 32.916 28.3897Z" fill="#193074"/>
-                  <rect x="26.8804" y="0.350098" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <rect x="14.1958" y="0.350098" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <rect x="4.18213" y="10.3643" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M0.871582 28.3895H16.8392C16.5 24.2774 13.0551 21.0459 8.85541 21.0459C4.65574 21.0459 1.21083 24.2774 0.871582 28.3895Z" fill="#193074"/>
-                  <rect x="20.2056" y="19.0428" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M16.8926 37.0682H32.8602C32.521 32.956 29.0761 29.7245 24.8764 29.7245C20.6767 29.7245 17.2318 32.956 16.8926 37.0682Z" fill="#193074"/>
-                </svg>
-              </div>
-            </div>
-            <div class="text-content">
-              <h4>{{ faculty.name }}</h4>
-              <h2>Courses’ list</h2>
-            </div>
-          </div>
-          <div class="search col">
-            <div class="search-wrapper">
-              <search  placeholder="Search user"/>
-            </div>
-          </div>
-          <div class="add-user d-flex justify-end col">
-
-          </div>
-        </div>
-      </div>
       <div class="tabular-users">
         <div class="table-wrapper mt-6" v-if="courses.length > 0">
 
@@ -52,7 +20,6 @@
 </template>
 
 <script>
-import Search from "../../../components/reusable/Search2";
 import TableHeader from "../../../components/reusable/ui/table-header";
 import TableUi from "../../../components/reusable/ui/table-ui";
 import apis from "../../../services/apis";
@@ -61,7 +28,7 @@ import moment from "moment";
 export default {
   //TODO using dynamic students from backend
 name: "FacultyCourses",
-  components: {TableUi, TableHeader, Search,},
+  components: {TableUi, TableHeader},
   data(){
     return{
       faculty:{},
--- src/views/faculties/faculty/Groups.vue
+++ src/views/faculties/faculty/Groups.vue
@@ -1,42 +1,6 @@
 <template>
 <div class="my-faculties bg-one d-flex justify-center">
   <div class="faculties-container  ">
-      <div class="header">
-        <div class="header-wrapper d-flex ">
-          <div class="heading col d-flex align-center">
-            <div class="icon">
-              <div class="icon-wrapper">
-                <svg xmlns="http://www.w3.org/2000/svg" width="49" height="38" viewBox="0 0 49 38" fill="none">
-                  <rect x="36.2266" y="10.3643" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M32.916 28.3897H48.8837C48.5444 24.2775 45.0995 21.046 40.8998 21.046C36.7002 21.046 33.2553 24.2775 32.916 28.3897Z" fill="#193074"/>
-                  <rect x="26.8804" y="0.350098" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <rect x="14.1958" y="0.350098" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <rect x="4.18213" y="10.3643" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M0.871582 28.3895H16.8392C16.5 24.2774 13.0551 21.0459 8.85541 21.0459C4.65574 21.0459 1.21083 24.2774 0.871582 28.3895Z" fill="#193074"/>
-                  <rect x="20.2056" y="19.0428" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M16.8926 37.0682H32.8602C32.521 32.956 29.0761 29.7245 24.8764 29.7245C20.6767 29.7245 17.2318 32.956 16.8926 37.0682Z" fill="#193074"/>
-                </svg>
-              </div>
-            </div>
-            <div class="text-content">
-              <h4>Faculty Details</h4>
-              <h2>Communication Design</h2>
-            </div>
-          </div>
-          <div class="search col">
-            <div class="search-wrapper">
-              <search  placeholder="Search user"/>
-            </div>
-          </div>
-          <div class="add-user d-flex justify-end col">
-            <div class="add-user-button">
-              <button-ui>
-                <template #content>EDIT FACULTY INFO</template>
-              </button-ui>
-            </div>
-          </div>
-        </div>
-      </div>
       <div class="tabular-users">
         <div class="table-wrapper mt-6">
           <div class="table-header">
@@ -75,8 +39,6 @@
 </template>
 
 <script>
-import buttonUi from '@/components/reusable/ui/button-ui'
-import Search from "../../../components/reusable/Search2";
 import TableHeader from "../../../components/reusable/ui/table-header";
 import TableUi from "../../../components/reusable/ui/table-ui";
 import tableActionBurner from '../../../components/reusable/ui/table-action-burner'
@@ -84,7 +46,7 @@ import tableActionBurner from '../../../components/reusable/ui/table-action-burn
 export default {
   //TODO add dynamic facalty groups
 name: "FacultyGroups",
-  components: {TableUi, TableHeader, Search, buttonUi, tableActionBurner},
+  components: {TableUi, TableHeader, tableActionBurner},
   data(){
     return{
       groups:[
