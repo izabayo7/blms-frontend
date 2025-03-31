15a98690084f5683e85a0864edd342fa3592b7ec
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 15:19:14 +0200

    adding child routing

--- src/views/faculties/faculty/Faculty.vue
+++ src/views/faculties/faculty/Faculty.vue
@@ -2,51 +2,6 @@
 <main class="faculty">
   <main class="faculty--wrapper">
 
-<!--    faculty page header-->
-    <section class="faculty--header header ">
-      <div class="header--wrapper d-flex">
-        <div class="heading col d-flex align-center">
-            <div class="icon">
-              <div class="icon-wrapper">
-                <svg xmlns="http://www.w3.org/2000/svg" width="49" height="38" viewBox="0 0 49 38" fill="none">
-                  <rect x="36.2266" y="10.3643" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M32.916 28.3897H48.8837C48.5444 24.2775 45.0995 21.046 40.8998 21.046C36.7002 21.046 33.2553 24.2775 32.916 28.3897Z" fill="#193074"/>
-                  <rect x="26.8804" y="0.350098" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <rect x="14.1958" y="0.350098" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <rect x="4.18213" y="10.3643" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M0.871582 28.3895H16.8392C16.5 24.2774 13.0551 21.0459 8.85541 21.0459C4.65574 21.0459 1.21083 24.2774 0.871582 28.3895Z" fill="#193074"/>
-                  <rect x="20.2056" y="19.0428" width="9.34646" height="9.34646" rx="4.67323" fill="#193074"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M16.8926 37.0682H32.8602C32.521 32.956 29.0761 29.7245 24.8764 29.7245C20.6767 29.7245 17.2318 32.956 16.8926 37.0682Z" fill="#193074"/>
-                </svg>
-              </div>
-            </div>
-            <div class="text-content">
-              <h4>Dashboard</h4>
-              <h2>Faculties List</h2>
-            </div>
-          </div>
-          <div class="search col">
-            <div class="search-wrapper">
-              <search  placeholder="Search user"/>
-            </div>
-          </div>
-          <div class="add-user d-flex justify-end col">
-            <div class="add-user-button">
-              <button-ui>
-                <template #content>Create faculty</template>
-              </button-ui>
-            </div>
-          </div>
-      </div>
-    </section>
-
-<!--    faculty page links/navbar-->
-    <section class="faculty--links links">
-      <section class="links--wrapper">
-        <subpage-navbar/>
-      </section>
-    </section>
-
 <!--    faculty page information/overview-->
     <section class="faculty--information information">
       <section class="faculty--information--wrapper d-flex ">
@@ -136,12 +91,9 @@
 </template>
 
 <script>
-import SubpageNavbar from "../../../components/reusable/subpage-navbar";
-import Search from "../../../components/reusable/Search2";
-import ButtonUi from "../../../components/reusable/ui/button-ui";
+
 export default {
 name: "Faculty",
-  components: {ButtonUi, Search, SubpageNavbar},
   data(){
     return{
       facultyId:this.$route.params.facultyId
