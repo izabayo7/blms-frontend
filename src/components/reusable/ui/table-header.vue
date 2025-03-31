84923a01f2bcd5cd7a043ad4cb8f2a47d17d4be7
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 13:57:19 +0200

    table header creation

---
+++ src/components/reusable/ui/table-header.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="my-table-header">
+  <div class="table-header-container">
+    <div class="actions">
+      
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name: "table-header"
+}
+</script>
+
+<style lang="scss" scoped>
+  .my-table{
+    .table-header-container{
+
+    }
+  }
+</style>
