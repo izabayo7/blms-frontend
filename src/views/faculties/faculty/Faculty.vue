d0985e1c5d4f0451f7bd524bc52cae65cadb7a34
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:47:52 +0200

    get faculties before setting header

--- src/views/faculties/faculty/Faculty.vue
+++ src/views/faculties/faculty/Faculty.vue
@@ -125,8 +125,8 @@ name: "Faculty",
   methods:{
   },
   async mounted(){
-      await this.$store.dispatch('faculties/changeHeader',{head:this.faculty.name,title:"Faculty Details"})
       await this.$store.dispatch("faculties/getFaculties",this.facultyId)
+      await this.$store.dispatch('faculties/changeHeader',{head:this.faculty.name,title:"Faculty Details"})
   }
 }
 </script>
