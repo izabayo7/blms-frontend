39f872f98aeb2ac8eb40e6967aa66ab6910ba262
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 16:41:29 +0200

    feat:data sampling

--- src/components/Live/OnlineUser.vue
+++ src/components/Live/OnlineUser.vue
@@ -2,12 +2,12 @@
 <div class="online-user">
   <div class="online-users--pic">
     <figure class="online-user--pic--wrapper">
-      <img src="https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png" alt="">
+      <img :src="user.img" :alt="`${user.name} profile picture`">
     </figure>
   </div>
   <div class="online-user--details">
-    <div class="online-user--details--name"></div>
-    <div class="online-user--details--attendency"></div>
+    <div class="online-user--details--name">{{user.name}}</div>
+    <div class="online-user--details--attendance">{{user.attendance}}</div>
   </div>
 </div>
 </template>
@@ -17,7 +17,11 @@ export default {
   name: "OnlineUser",
   data(){
     return {
-      user:{}
+      user:{
+        img:'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png',
+        name:'Ntwari liberi',
+        attendance:89,
+      }
     }
   }
 }
