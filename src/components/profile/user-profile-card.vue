ec2e6f2338ce908ff5f149ad2bd099fa5e6c620a
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 17:59:32 +0200

    finishing card

--- src/components/profile/user-profile-card.vue
+++ src/components/profile/user-profile-card.vue
@@ -1,14 +1,20 @@
 <template>
 <!--  upc = user profile card-->
+
+<!--  user picture card-->
   <section class="upc">
-    <section class="upc--wrapper">
-      <article class="upc--information">
-        <article class="upc--information--picture">
+    <section class="upc--wrapper d-flex flex-column">
+
+<!--      user picture card information row-->
+      <article class="upc--information d-flex ">
+        <article class="upc--information--picture px-5 py-5">
+<!--          user profile picture-->
           <figure class="upc--information--picture--img">
             <img src="https://i0.wp.com/www.mobileworldlive.com/wp-content/uploads/2015/10/Dorsey-iamge.png?w=150&ssl=1" alt="user profile picture">
           </figure>
         </article>
-        <article class="upc--information--data">
+<!--        user information-->
+        <article class="upc--information--data px-5 py-5">
           <div class="upc--information--data--name">
             <h2>Umuhoza solange</h2>
           </div>
@@ -23,12 +29,16 @@
           </div>
         </article>
       </article>
-      <article class="upc--cat--buttons">
+
+<!--      user picture card call to action buttons row-->
+      <article class="upc--cat--buttons d-flex justify-space-between">
+<!--        msg button-->
         <div class="upc--cat--message">
-          <button-ui> <template #content>Message</template></button-ui>
+          <button-ui fill class-list="px-16 py-3"> <template #content>Message</template></button-ui>
         </div>
+<!--        call button-->
         <div class="upc--cat--call">
-          <button-ui> <template #content>Call</template></button-ui>
+          <button-ui class-list="px-16 py-3" category="warn" fill> <template #content>Call</template></button-ui>
         </div>
       </article>
     </section>
@@ -43,6 +53,64 @@ name: "user-profile-card",
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.upc{
+  max-width: 24rem;
+  &--wrapper{
 
+  }
+
+  &--information{
+    &--picture{
+      &--img{
+        img{
+          border-radius: 50%;
+          width:6rem;
+          height:6rem;
+          object-fit: cover;
+          border:5px solid lighten($primary,60)
+        }
+      }
+    }
+
+    &--data{
+      &--name{
+        h2{
+          font-weight: 500;
+        }
+      }
+
+      &--type{
+        h5{
+          font-weight: 400;
+          color:lighten($font,20);
+        }
+      }
+
+      &--faculty{
+        margin-top: 1rem;
+        h3{
+          color:$color-one;
+        }
+      }
+
+      &--email{
+        p{
+          font-size:.8rem;
+        }
+      }
+    }
+  }
+
+  &--cat{
+    &--buttons{
+
+    }
+    &--call{
+      .orange-bg{
+        background-color: $color-one;
+      }
+    }
+  }
+}
 </style>
