b1e1ed25a6f6cc6a1a2ca729cb6ba80b36432ac9
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 15:30:46 +0200

    getting user infromation

--- src/mixins/user-simple-card.mixin.js
+++ src/mixins/user-simple-card.mixin.js
@@ -1,16 +1,14 @@
 export default{
     methods:{
-        mouseOnPic(e,userId,cardId){
+        mouseOnPic(e,userName,cardId){
             const card = document.getElementById(cardId)
-          this.timeout = setTimeout(() => {
-            card.style.display = "block"
-            card.style.top = e.clientY + "px";
-            card.style.left = e.clientX + "px";
 
-            console.log(userId)
-
-            // console.log(e.clientY)
-          }, 1500)
+            this.timeout = setTimeout(() => {
+                this.$store.dispatch('users/getUserByUsername',userName)
+                card.style.display = "block"
+                card.style.top = e.clientY + "px";
+                card.style.left = e.clientX + "px";
+            }, 1500)
 
         },
         mouseOutPic(e,cardId){
