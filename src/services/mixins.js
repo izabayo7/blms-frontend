4ca26020da4fa82a2a05e602c98505184a372028
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 16:39:56 +0200

    new: cropper mixin

--- src/services/mixins.js
+++ src/services/mixins.js
@@ -1,4 +1,6 @@
-exports.chatMixins = {
+import {emit} from "./event_bus";
+
+const chatMixins = {
     methods:{
         //scroll chat to button
         scrollChatToBottom(){
@@ -7,3 +9,33 @@ exports.chatMixins = {
         },
     }
 }
+
+const cropper = {
+    methods:{
+        readURL(input) {
+            const self = this;
+            input = input.target;
+            if (input.files && input.files[0]) {
+                let reader = new FileReader();
+
+                reader.onload = function (e) {
+                    self.img = e.target.result;
+                    emit("new-image-loaded"); // to tell image cropper that new image was added
+                };
+
+                reader.readAsDataURL(input.files[0]); // convert to base64 string
+            }
+        },
+        imageCropped(img) {
+            const image = document.getElementById("preview");
+            image.src = img;
+            this.profile = img;
+        },
+        handleFileUpload(e) {
+            this.readURL(e);
+            this.profile = this.$refs.file.files[0];
+        }
+    }
+}
+
+export {cropper,chatMixins}
