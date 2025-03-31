e9e74097807401b8400b2f3c0834b0a3915fc631
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 13:21:58 +0200

    fixed user-simple-card component behaviour

--- src/mixins/user-simple-card.mixin.js
+++ src/mixins/user-simple-card.mixin.js
@@ -9,17 +9,18 @@ export default {
                 card.style.display = "block"
                 card.style.top = e.clientY + "px";
                 card.style.left = e.clientX + "px";
-            }, 500)
+            }, 1500)
 
         },
         mouseOutPic(e, cardId) {
+            console.log("hano",e)
             if (e)
-                if (!e || e.toElement ? e.toElement.className !== "user-card--wrapper" : false) {
+                if (e.relatedTarget ? (e.relatedTarget.id !== cardId && e.relatedTarget.className !== "user-card--wrapper") : false) {
                     clearTimeout(this.timeout)
                     const card = document.getElementById(cardId)
                     setTimeout(() => {
                         card.style.display = "none";
-                    }, 500)
+                    }, 1000)
                 }
         },
     }
