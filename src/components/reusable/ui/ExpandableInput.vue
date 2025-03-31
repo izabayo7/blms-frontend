4adb195cbda6d6c1906f8a0d6a28ad30f50722f2
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 10:30:52 +0200

    feat(input): creation of expandable input

---
+++ src/components/reusable/ui/ExpandableInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <span role="textbox" contenteditable type="text">
+  </span>
+</template>
+
+<script>
+export default {
+  name: "ExpandableInput"
+}
+</script>
+
+<style scoped>
+
+</style>
