15a22f32c7af976518b8383e10a4d72a0671bbb3
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 10:23:34 +0200

    switched action buttons in admin dashboard

--- src/views/dashboard/new/admin.vue
+++ src/views/dashboard/new/admin.vue
@@ -297,14 +297,14 @@
           <v-col class="col-12 col-lg-10 pt-0 pl-0 recent-joined">
             <router-link class="more text-right" to="/users">More ...</router-link>
             <div class="mt-5 d-flex">
+              <button class="lower_buttons" @click="showFacultyModal = true">
+                New Faculty
+              </button>
               <button
                   class="lower_buttons mr-2"
                   @click="showInviteUsers = true"
               >
                 Invite users
-              </button>
-              <button class="lower_buttons" @click="showFacultyModal = true">
-                New Faculty
               </button>
             </div>
           </v-col>
