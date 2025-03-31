2e9f5cc7665242e170388b14edb379ee844ef2e2
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:03:21 +0200

    new: initiating component

---
+++ src/components/Live/DiscussionHead.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="my-discussion-head">
+        <div class="discussion-head-container">
+            <h1>Economics Basics Part II</h1>
+            <div class="live">
+                <span>live</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DiscussionHead"
+    }
+</script>
+
+<style scoped>
+
+</style>
