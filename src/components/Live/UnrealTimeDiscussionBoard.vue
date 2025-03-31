fea996f2320acbf0749d5af9ab96993775c1ba51
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 14:29:50 +0200

    new: get comments

--- src/components/Live/UnrealTimeDiscussionBoard.vue
+++ src/components/Live/UnrealTimeDiscussionBoard.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="my-unreal-time-discussion-board">
         <div class="unreal-time-discussion-bord-container px-3 py-5">
-            <div class="head mb-5">
+            <div class="head mb-5" v-if="head_visible">
                 <discussion-head content="Economics Basics part II" :live="true"/>
             </div>
             <div class="student-new-comment mb-5">
-                <student-new-comment-with-photo />
+                <student-new-comment-with-photo  />
             </div>
             <div class="unreal-time-discussions-holder">
                 <!--                <div class="new-comment">-->
                 <!--                    <new-comment />-->
                 <!--                </div>-->
                 <div class="unreal-time-discussions">
-                    <discussion />
+                    <div class="discussion" v-for="comment in comments" :key="comment._id">
+                        <discussion :content="comment"/>
+                    </div>
                 </div>
 
             </div>
@@ -26,21 +28,41 @@
     import Discussion from "./Discussion";
     import DiscussionHead from "./DiscussionHead";
     import StudentNewCommentWithPhoto from "./StudentNewCommentWithPhoto";
-
+    import api from '@/services/apis'
 
     export default {
         name: "UnrealTimeDiscussionBoard",
+        props:{
+            head_visible:{default:false,type:Boolean,}
+        },
+        data(){
+            return {
+                comments:[]
+            }
+        },
         components:{
             StudentNewCommentWithPhoto,
             // NewComment,
             Discussion,
             DiscussionHead
+        },
+        methods:{
+            async get_comments(){
+                const comments = await api.get("comment")
+                this.comments = comments.data.data
+                console.log(this.comments)
+            },
+
+        },
+        mounted() {
+            this.get_comments()
         }
     }
 </script>
 
 <style lang="scss" scoped>
 .my-unreal-time-discussion-board{
+    width: 100%;
     .unreal-time-discussion-bord-container{
         background-color: $main;
     }
