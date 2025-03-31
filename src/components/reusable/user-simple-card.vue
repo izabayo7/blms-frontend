4244d5945c25b342483e52d7fbc9adf63882ec78
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 14:25:22 +0200

    finalising user simple card

--- src/components/reusable/user-simple-card.vue
+++ src/components/reusable/user-simple-card.vue
@@ -1,40 +1,40 @@
 <template>
 <article class="user-card">
   <article class="user-card--wrapper">
-    <div class="user-card--row user-card--info user-card--row__1">
-      <div class="user-card--info--col user-card--info--names-and-type user-card--info--col__1">
+    <div class="user-card--row user-card--info user-card--row__1 d-flex">
+      <div class="user-card--info--col user-card--info--names-and-type user-card--info--col__1 col-8">
         <h2> <slot name="name"></slot> </h2>
-        <h4> <slot name="type"></slot> </h4>
+        <h6> <slot name="type"></slot> </h6>
       </div>
-      <div class="user-card--info--col user-card--info--pic user-card--info--col__2">
+      <div class="user-card--info--col user-card--info--pic user-card--info--col__2 col-4">
         <div class="image--wrapper">
           <slot name="image"></slot>
         </div>
       </div>
     </div>
-    <div class="user-card--row user-card--actions user-card--row__2">
-      <div class="user-card--actions--col user-card--actions--col__1">
+    <div class="user-card--row user-card--actions user-card--row__2 d-flex">
+      <div class="user-card--actions--col user-card--actions--col__1 ">
         <router-link to="">
-          <button class="user-card--actions--visit-profile">
-            <span>
-              <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7 0C3.136 0 0 3.136 0 7C0 10.864 3.136 14 7 14C10.864 14 14 10.864 14 7C14 3.136 10.864 0 7 0ZM7 2.1C8.162 2.1 9.1 3.038 9.1 4.2C9.1 5.362 8.162 6.3 7 6.3C5.838 6.3 4.9 5.362 4.9 4.2C4.9 3.038 5.838 2.1 7 2.1ZM7 12.04C5.25 12.04 3.703 11.144 2.8 9.786C2.821 8.393 5.6 7.63 7 7.63C8.393 7.63 11.179 8.393 11.2 9.786C10.297 11.144 8.75 12.04 7 12.04Z" fill="#193074"/>
-              </svg>
-            </span>
-            <span>Visit profile</span>
-          </button>
+          <button-ui :class-list="'px-2 py-2'"  class="user-card--actions--button__visit-profile user-card--actions--button">
+            <template #content>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M20 22H4v-2a5 5 0 0 1 5-5h6a5 5 0 0 1 5 5v2zm-8-9a6 6 0 1 1 0-12 6 6 0 0 1 0 12z" fill="rgba(255,255,255,1)"/></svg>
+              </span>
+              <span>Visit profile</span>
+            </template>
+          </button-ui>
         </router-link>
       </div>
       <div class="user-card--actions--col user-card--actions--col__2">
         <router-link to="">
-          <button class="user-card--actions--message">
-            <span>
-              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M18 9.55557C18.003 10.7288 17.7289 11.8861 17.2 12.9333C16.5728 14.1882 15.6086 15.2437 14.4155 15.9816C13.2223 16.7195 11.8473 17.1106 10.4444 17.1111C9.27122 17.1142 8.11387 16.8401 7.06666 16.3111L2 18L3.68889 12.9333C3.15994 11.8861 2.88583 10.7288 2.88889 9.55557C2.88943 8.15269 3.28054 6.77766 4.01841 5.58451C4.75629 4.39135 5.81178 3.42719 7.06666 2.80002C8.11387 2.27107 9.27122 1.99697 10.4444 2.00003H10.8889C12.7416 2.10224 14.4916 2.88426 15.8037 4.19634C17.1157 5.50843 17.8978 7.25837 18 9.11113V9.55557Z" stroke="#193074" stroke-width="2.29139" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
-            </span>
-            <span>Message</span>
-          </button>
+          <button-ui :class-list="'px-2 py-2'" class="user-card--actions--button__message user-card--actions--button">
+            <template #content>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7.291 20.824L2 22l1.176-5.291A9.956 9.956 0 0 1 2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.956 9.956 0 0 1-4.709-1.176zm.29-2.113l.653.35A7.955 7.955 0 0 0 12 20a8 8 0 1 0-8-8c0 1.334.325 2.618.94 3.766l.349.653-.655 2.947 2.947-.655z" fill="rgba(255,255,255,1)"/></svg>
+              </span>
+              <span>Message</span>
+            </template>
+          </button-ui>
         </router-link>
       </div>
     </div>
@@ -43,11 +43,83 @@
 </template>
 
 <script>
+import ButtonUi from "./ui/button-ui";
 export default {
-  name: "user-simple-card"
+  name: "user-simple-card",
+  components: {ButtonUi}
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.user-card{
+  width:20rem;
 
+  &--wrapper{
+    background-color:$main;
+    padding:1rem;
+    border-radius:.55rem;
+  }
+
+  &--row{
+
+  }
+
+  &--info{
+    &--names-and-type{
+      h2{
+        font-size:1rem;
+        color:$primary;
+      }
+      h6{
+        font-size:.7rem;
+        color: $grayish;
+      }
+    }
+
+    &--col{
+
+      .image--wrapper{
+        display:flex;
+        justify-content: center;
+        align-content: center;
+
+        img{
+          width:4rem;
+          height:4rem;
+          border-radius:50%;
+          border:3.5px solid $primary;
+        }
+      }
+
+    }
+  }
+
+  &--actions{
+    display:flex;
+    justify-content: space-between;
+
+    &--col{
+      &__1{
+        a{
+          display:block;
+          width:10rem;
+        }
+      }
+      &__2{
+
+      }
+    }
+
+    &--button{
+      &__visit-profile{
+
+      }
+
+      &__message{
+
+      }
+    }
+
+  }
+}
 </style>
