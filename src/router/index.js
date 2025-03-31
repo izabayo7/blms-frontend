846593d9ad1507100accd6548e40cf53394ef541
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 10:05:48 +0200

    redirecting user to /unautorized if he try something drammatic

--- src/router/index.js
+++ src/router/index.js
@@ -286,7 +286,7 @@ router.beforeEach((to, from, next) => {
             })
         }
         if (to.meta.allowedUsers && !to.meta.allowedUsers.includes(store.state.user.user.category.name))
-            next({path: '/wambebaweeeeee'})
+            next({path: '/unauthorized'})
         next()
     }
 })
