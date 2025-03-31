5b2d9d8db1720a9c20aeb1845c0832bc067d4d84
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 16:12:45 +0200

    new: render reply text area;

--- src/components/Live/Discussion.vue
+++ src/components/Live/Discussion.vue
@@ -13,14 +13,14 @@
                     <div class="comment__text">Who can support you without having
                         the idea ?
                     </div>
-                    <div class="comment__reply-icon">
+                    <div class="comment__reply-icon" v-if="!commenting" @click="commenting = !commenting">
                         <div class="inner-icon">
                             <div class="icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0H24V24H0z"/><path d="M11 20L1 12l10-8v5c5.523 0 10 4.477 10 10 0 .273-.01.543-.032.81C19.46 16.95 16.458 15 13 15h-2v5z"/></svg>
                             </div><div class="text"><span>reply</span></div>
                         </div>
                     </div>
-                    <div class="reply-comment">
+                    <div class="reply-comment" v-else>
                         <reply-comment />
                     </div>
                 </div>
@@ -41,6 +41,11 @@
         },
         computed:{
             ...mapGetters('user',['user'])
+        },
+        data(){
+            return{
+                commenting:false
+            }
         }
     }
 </script>
