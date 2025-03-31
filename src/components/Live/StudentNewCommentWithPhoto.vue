d13133bbe3df62a65f4524c779af53901c722485
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 16:43:24 +0200

    new: comment object

--- src/components/Live/StudentNewCommentWithPhoto.vue
+++ src/components/Live/StudentNewCommentWithPhoto.vue
@@ -6,7 +6,7 @@
                     <v-avatar size="30">{{ user_full_names | computeText}} </v-avatar>
 
                 </div>
-                <div class="input mx-3 px-3 py-1"><input type="text" placeholder="write something.."></div>
+                <div class="input mx-3 px-3 py-1"><input v-model="comment" type="text" placeholder="write something.."></div>
                 <div class="send">
                     <div class="icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"/></svg></div>
                 </div>
@@ -21,9 +21,23 @@
 
     export default {
         name: "StudentNewCommentWithPhoto",
+        data(){
+            return{
+                comment:''
+            }
+        },
         computed:{
-            ...mapGetters("user", ['user_full_names'])
-        }
+            ...mapGetters("user", ['user_full_names','username']),
+            ...mapGetters("courses", ['selectedChapter']),
+            comment_object(){
+                return {
+                    sender:this.username,
+                    target:{type:"chapter",id:this.selectedChapter},
+                    content:this.comment
+                }
+            }
+        },
+
     }
 </script>
 
