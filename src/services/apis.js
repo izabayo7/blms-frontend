111bcbc29d347627c6e884653c7e110e6576fbaf
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 16:49:09 +0200

    new: fn to variable instance and interceptors

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,40 +1,46 @@
 import Api from './server'
-import axios from 'axios'
-const a = axios;
+// import Vue from "vue";
+
+//get token
+const {jwt:token} = JSON.parse(localStorage.getItem('vue-session-key'))
+
 
 /* interceptors */
-a.interceptors.response.use((response) => {
+Api.interceptors.response.use(function (response) {
+
     return response
-}, (error) => {
+}, function (error){
+    console.log(error)
     return new Promise.reject(error)
 })
-a.interceptors.request.use((config) => {
-    console.log('in request fullfilled', config)
+
+
+Api.interceptors.request.use((config) => {
+
     return config
-}, error => {
-    console.log('in request error', (error))
-    return new Promise.reject(error)
 })
+
+
 
 export default {
     // users login
     login(body) {
-        return Api().post(`/user/login`, body)
+        return Api.post(`/user/login`, body)
     },
     // get requests
     get(url) {
-        return Api().get(`/${url}`)
+        return Api.get(`/${url}`)
     },
     // post requests
     create(path, body, config) {
-        return Api().post(`/${path}`, body, config)
+        return Api.post(`/${path}`, body, config)
     },
     // put requests
     update(path, id, body, config) {
-        return Api().put(`/${path}/${id}`, body, config)
+        return Api.put(`/${path}/${id}`, body, config)
     },
     // delete requests
     delete(path, id) {
-        return Api().delete(`/${path}/${id}`)
+        return Api.delete(`/${path}/${id}`)
     },
 }
