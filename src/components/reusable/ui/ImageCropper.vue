77b6cd5da11aeee19f3205b746f27d5298a023a9
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 16:16:49 +0200

    finalising image cropper

--- src/components/reusable/ui/ImageCropper.vue
+++ src/components/reusable/ui/ImageCropper.vue
@@ -1,19 +1,21 @@
 <template>
-<div class="cropper-container" id="imageCropper" v-if="visible">
-  <div class="inner-cropper" >
-    <div class="img">
-      <div class="cropper-holder">
-        <h3 v-if="imgLoaded">Crop image</h3>
-        <cropper @change="changed" ref="cropper" class="cropper" :src="img" :stencil-props="{aspectRatio: 1}" ></cropper>
+<div class="shade">
+  <div class="cropper-container" id="imageCropper" v-if="visible">
+    <div class="inner-cropper" >
+      <div class="img">
+        <div class="cropper-holder">
+          <h3 v-if="imgLoaded">Crop image</h3>
+          <cropper @change="changed" ref="cropper" class="cropper" :src="img" :stencil-props="{aspectRatio: 1}" ></cropper>
+        </div>
+        <div class="preview" v-show="imgLoaded">
+          <h3>Image preview</h3>
+          <img ref="preview" src="" alt="">
+        </div>
       </div>
-      <div class="preview" v-show="imgLoaded">
-        <h3>Image preview</h3>
-        <img ref="preview" src="" alt="">
+      <div class="btn">
+        <button class="done" @click="done">Save changes</button>
+        <button class="cancel" @click="cancel">Cancel</button>
       </div>
-    </div>
-    <div class="btn">
-      <button class="done" @click="done">Save changes</button>
-      <button class="cancel" @click="cancel">Cancel</button>
     </div>
   </div>
 </div>
@@ -33,7 +35,7 @@ export default {
   },
   data(){
     return{
-      visible:true,
+      visible:false,
       imgLoaded:false
     }
   },
@@ -82,107 +84,109 @@ export default {
   }
 }
 
-.cropper-container{
-  position: absolute;
-  top:50%;
-  left:50%;
-  transform: translate(-50%,-50%);
-  padding:1rem ;
-  height: fit-content;
-  min-width: 50%;
-  max-width: 80%;
-  width: fit-content;
-  background-color: $main;
-  z-index: 101;
-  box-shadow:  0 0 20px 4px #4ac1c62e;
+.shade{
 
-  .btn{
-    display: flex;
-    justify-content: center;
+  .cropper-container{
+    position: absolute;
+    top:50%;
+    left:50%;
+    transform: translate(-50%,-50%);
+    padding:1rem ;
+    height: fit-content;
+    min-width: 50%;
+    max-width: 80%;
+    width: fit-content;
+    background-color: $main;
+    z-index: 101;
+    box-shadow:  0 0 20px 4px #4ac1c62e;
 
-    button{
-      min-width: 10rem;
-      padding:1rem 2rem;
-      margin:1rem;
-      width: fit-content;
-
-      &.done{
-        box-shadow: 0 0 10px 1px lighten($success,30);
-        background-color: $success;
-        color:$main;
-
-        &:hover{
-          background-color: lighten($success,10);
-        }
-
-      }
-      &.cancel{
-        border: 2px solid lighten($font,20);
-
-        &:hover{
-          background-color:lighten( $secondary,6);
-        }
-      }
-
-    }
-  }
-  .inner-cropper{
-    height: 100%;
-    width: 100%;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-self: flex-end;
-
-
-    .img{
+    .btn{
       display: flex;
-      flex-wrap: wrap;
-      width: 100%;
       justify-content: center;
 
+      button{
+        min-width: 10rem;
+        padding:1rem 2rem;
+        margin:1rem;
+        width: fit-content;
 
-      h3{
-        text-align: center;
-        padding: .5rem;
+        &.done{
+          background-color: $success;
+          color:$main;
+
+          &:hover{
+            background-color: lighten($success,10);
+          }
+
+        }
+        &.cancel{
+          background-color: $secondary;
+
+          &:hover{
+            background-color:darken( $secondary,5);
+          }
+        }
+
+      }
+    }
+    .inner-cropper{
+      height: 100%;
+      width: 100%;
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-self: flex-end;
+
+
+      .img{
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        justify-content: center;
+
+
+        h3{
+          text-align: center;
+          padding: .5rem;
+        }
+
+        .cropper-holder{
+          padding: 1rem;
+          max-width: 40rem;
+          box-sizing: border-box;
+          width:80%;
+          img{
+            width: 100% !important;
+            object-fit: contain;
+          }
+        }
+
+        .preview{
+          box-sizing: border-box;
+          align-self: center;
+          padding: 1rem;
+          min-width: 10rem;
+          width: 20%;
+          img{
+            width: 100%;
+          }
+        }
+
       }
 
       .cropper-holder{
         padding: 1rem;
-        max-width: 40rem;
-        box-sizing: border-box;
-        width:80%;
-        img{
-          width: 100% !important;
-          object-fit: contain;
-        }
-      }
+        .cropper{
 
-      .preview{
-        box-sizing: border-box;
-        align-self: center;
-        padding: 1rem;
-        min-width: 10rem;
-        width: 20%;
-        img{
-          width: 100%;
-        }
-      }
-
-    }
-
-    .cropper-holder{
-      padding: 1rem;
-      .cropper{
-
-        .vue-advanced-cropper__image{
-          width:100%;
-          object-fit: contain;
+          .vue-advanced-cropper__image{
+            width:100%;
+            object-fit: contain;
+          }
         }
       }
     }
+
   }
-
 }
 </style>
