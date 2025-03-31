c65a616264673e26b0e6d56b1fcbb294b04f37a4
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 13:41:18 +0200

    added faculty empty state holder

--- src/views/faculties/index.vue
+++ src/views/faculties/index.vue
@@ -54,8 +54,8 @@
 
 
       <!--    table of faculties-->
-      <div class="tabular-faculties">
-        <div class="table-wrapper" v-if="formatedFaculties.length > 0">
+      <div class="tabular-faculties" v-if="formatedFaculties.length < 0">
+        <div class="table-wrapper">
 
           <!--          list of faculties in table-->
           <div class="table">
@@ -84,6 +84,23 @@
               </template>
             </table-ui>
           </div>
+        </div>
+      </div>
+      <div v-else class="empty-faculties">
+        <div class="wrapper">
+          <div class="icon">
+            <svg width="50" height="56" viewBox="0 0 50 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M25.0001 55.7692C24.921 55.7692 24.8417 55.7623 24.7635 55.7485C10.1831 53.1912 0 44.4159 0 34.4085V1.3463C0 0.602805 0.608023 0 1.35795 0H48.642C49.392 0 50 0.602805 50 1.3463V34.4085C50 44.4159 39.8169 53.1912 25.2365 55.7485C25.1583 55.7623 25.0792 55.7692 25.0001 55.7692Z" fill="#BABABC"/>
+              <path d="M25.5 16.1852L10 11V33.8148L25.5 39M25.5 16.1852L41 11V32.7778L25.5 39M25.5 16.1852V39M30.6667 21.3704L36.8667 19.2963M30.6667 27.5926L36.8667 25.5185M20.3333 27.5926L14.1333 25.5185M20.3333 21.3704L14.1333 19.2963" stroke="#F7F7F7" stroke-width="1.39569" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </div>
+          <div class="title">No faculty</div>
+          <div class="subtitle">Your school has no faculty for now
+            create one by clicking on
+          </div>
+          <button @click="showFacultyModal = true" class="action">
+            Create Faculty
+          </button>
         </div>
       </div>
     </div>
@@ -155,6 +172,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/sass/imports/tableEmptyState';
 .my-faculties {
   height: 100%;
   width: 100%;
