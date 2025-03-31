7bc4538fa004095e909c9260101d3ef103f39a88
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 09:12:59 +0200

    added message member option in group settings

--- src/components/messages/Group-member.vue
+++ src/components/messages/Group-member.vue
@@ -7,10 +7,10 @@
       </v-avatar>
     </div>
     <p class="names">
-      {{ `${member.data.sur_name} ${member.data.other_names}` }}
+      {{ member.data.user_name == $store.state.user.user.user_name ? 'You' : `${member.data.sur_name} ${member.data.other_names}` }}
     </p>
     <div class="icons">
-      <div class="m-message" v-if="!IamTheOwner">
+      <div class="m-message" v-if="!IamTheOwner" @click="$router.push(`/messages/${member.data.user_name}`)">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M5.763 17H20V5H4v13.385L5.763 17zm.692 2L2 22.5V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H6.455z"/></svg>
       </div>
       <div class="remove" v-if="!IamTheOwner">
@@ -45,7 +45,7 @@ export default {
   },
   computed:{
     IamTheOwner(){
-      return this.member.id === this.$store.state.user.user._id
+      return this.member.data.user_name === this.$store.state.user.user.user_name
     }
   }
 }
